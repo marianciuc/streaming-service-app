bea32332b9fc10a16ba9dc746d47be7aa3239009
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-13 20:59:55 +0200

    Refactor ContentRepository to use JpaSpecificationExecutor
    Remove unused imports and simplify the repository interface by eliminating the specific title-based search method. This enhances code maintainability and leverages `JpaSpecificationExecutor` for flexible and dynamic queries.

--- services/content-service/src/main/java/com/mv/streamingservice/content/repository/ContentRepository.java
+++ services/content-service/src/main/java/com/mv/streamingservice/content/repository/ContentRepository.java
@@ -1,16 +1,14 @@
 package com.mv.streamingservice.content.repository;
 
 import com.mv.streamingservice.content.entity.Content;
-import com.mv.streamingservice.content.entity.Genre;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
 public interface ContentRepository extends JpaRepository<Content, UUID>, JpaSpecificationExecutor<Content> {
-    Page<Content> findByTitleContainingIgnoreCase(String title, Pageable pageable);
-
     Page<Content> findAll(Specification<Content> spec, Pageable pageable);
 }
