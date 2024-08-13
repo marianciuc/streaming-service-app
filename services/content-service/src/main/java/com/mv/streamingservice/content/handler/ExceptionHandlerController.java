f9db0b8480fe7289826cff88883927b77f47d18b
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-13 23:23:48 +0200

    Add exception handling for NotFoundException
    Introduce NotFoundException class and add its handler to ExceptionHandlerController. This handler responds with a 404 status code and a custom exception message when the exception is thrown.

--- services/content-service/src/main/java/com/mv/streamingservice/content/handler/ExceptionHandlerController.java
+++ services/content-service/src/main/java/com/mv/streamingservice/content/handler/ExceptionHandlerController.java
@@ -1,9 +1,13 @@
 package com.mv.streamingservice.content.handler;
 
 import com.mv.streamingservice.content.dto.response.ExceptionResponse;
+import com.mv.streamingservice.content.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
 
@@ -23,5 +27,12 @@ public class ExceptionHandlerController {
      */
     private ResponseEntity<ExceptionResponse> constructResponseEntity(HttpStatus status, String message) {
         return ResponseEntity.status(status).body(new ExceptionResponse(message, LocalDateTime.now()));
+    }
+
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    protected ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException ex, WebRequest request) {
+        return constructResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 }
