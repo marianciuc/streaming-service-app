e7bcda61ca63aa93ff9820c72691a3eee4c8beb5
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-13 20:55:05 +0200

    Add Javadoc to GenericMapper interface
    Introduce Javadoc comments to the GenericMapper interface to clarify its purpose and usage. The comments include detailed descriptions of the interface and its generic type parameters, improving code readability and maintainability.

--- services/content-service/src/main/java/com/mv/streamingservice/content/mappers/GenericMapper.java
+++ services/content-service/src/main/java/com/mv/streamingservice/content/mappers/GenericMapper.java
@@ -1,5 +1,13 @@
 package com.mv.streamingservice.content.mappers;
 
+/**
+ * The GenericMapper interface represents a mapping contract between entity objects and DTO (Data Transfer Object) objects.
+ * It provides methods to convert an entity object to a DTO object and vice versa.
+ *
+ * @param <E> the entity type
+ * @param <D> the DTO type
+ * @param <F> the type used for converting the DTO back to the entity type
+ */
 public interface GenericMapper<E, D, F>{
     D toDto(E entity);
     E toEntity(F dto);
