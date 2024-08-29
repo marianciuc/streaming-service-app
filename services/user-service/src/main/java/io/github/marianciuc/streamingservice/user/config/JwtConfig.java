18d4541e16a3119248d5ff56f3d17677ced6e7f6
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-29 15:02:36 +0200

    Add UserService and JwtFilter to JwtConfig
    Refactor JwtConfig to include UserService and JsonWebTokenFilter beans. This change also updates the jsonWebTokenService bean to use the implementation class JsonWebTokenServiceImpl.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/config/JwtConfig.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/config/JwtConfig.java
@@ -1,6 +1,10 @@
 package io.github.marianciuc.streamingservice.user.config;
 
+import io.github.marianciuc.jwtsecurity.filters.JsonWebTokenFilter;
 import io.github.marianciuc.jwtsecurity.service.JsonWebTokenService;
+import io.github.marianciuc.jwtsecurity.service.UserService;
+import io.github.marianciuc.jwtsecurity.service.impl.JsonWebTokenServiceImpl;
+import io.github.marianciuc.jwtsecurity.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,9 +20,20 @@ public class JwtConfig {
     @Value("${security.jwt.token.secret-key}")
     private String secretKey;
 
+    @Value("${spring.application.name}")
+    private String serviceName;
 
     @Bean
     JsonWebTokenService jsonWebTokenService(){
-        return new JsonWebTokenService(secretKey, accessExpiration, refreshExpiration);
+        return new JsonWebTokenServiceImpl(serviceName, secretKey, accessExpiration, refreshExpiration);
+    }
+
+    @Bean
+    UserService userService (){
+        return new UserServiceImpl();
+    }
+    @Bean
+    JsonWebTokenFilter jsonWebTokenFilter(){
+        return new JsonWebTokenFilter(jsonWebTokenService(), userService());
     }
 }
