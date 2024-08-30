cbf12ef8822715879a1d07a1df4ecf4e05dfec4d
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 21:21:31 +0200

    Add MediaServiceApplication with Spring Boot setup
    This commit introduces the MediaServiceApplication class for the media service. It includes Spring Boot initialization and enables discovery client and JPA auditing for the microservice. This setup is essential for configuring and running the media service application.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/MediaServiceApplication.java
@@ -0,0 +1,17 @@
+package io.github.marianciuc.streamingservice.media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableJpaAuditing
+public class MediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaServiceApplication.class, args);
+    }
+
+}
