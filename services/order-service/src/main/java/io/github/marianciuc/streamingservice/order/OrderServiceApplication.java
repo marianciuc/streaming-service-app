b9144b576956d2435beb7303d667ecf4e3b8c71d
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:33:46 +0200

    Add various Spring Boot annotations for service enhancements
    Included annotations to enable Feign clients, service discovery, JPA auditing, and scheduling. These changes improve the application's integration capabilities, database auditing functionality, and task scheduling features.

--- services/order-service/src/main/java/io/github/marianciuc/streamingservice/order/OrderServiceApplication.java
+++ services/order-service/src/main/java/io/github/marianciuc/streamingservice/order/OrderServiceApplication.java
@@ -1,9 +1,18 @@
 package io.github.marianciuc.streamingservice.order;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@EnableJpaAuditing
+@EnableScheduling
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
