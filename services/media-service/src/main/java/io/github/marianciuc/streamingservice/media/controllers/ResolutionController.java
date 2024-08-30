6e060ca4d7ce142aba66501582f89fb27e931193
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 21:52:23 +0200

    Add permission checks to ResolutionController endpoints
    The methods in ResolutionController now require either ROLE_SERVICE or ROLE_ADMIN authorities to be accessed, ensuring that only authorized users can perform operations like retrieving, creating, deleting, updating, and listing resolution data. This enhances the security of the media service by validating user roles before executing any method.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/controllers/ResolutionController.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/controllers/ResolutionController.java
@@ -13,6 +13,7 @@ import io.github.marianciuc.streamingservice.media.services.ResolutionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,27 +27,32 @@ public class ResolutionController {
     private final ResolutionService resolutionService;
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_SERVICE', 'ROLE_ADMIN')")
     private ResponseEntity<ResolutionDto> getResolution(@RequestParam("id") UUID id) {
         return ResponseEntity.ok(resolutionService.getResolutionById(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_SERVICE', 'ROLE_ADMIN')")
     private ResponseEntity<ResolutionDto> createResolution(@RequestBody @Valid ResolutionDto request) {
         return ResponseEntity.ok(resolutionService.createResolution(request));
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_SERVICE', 'ROLE_ADMIN')")
     private ResponseEntity<Void> deleteResolution(@RequestParam("id") UUID id) {
         resolutionService.deleteResolution(id);
         return ResponseEntity.ok().build();
     }
 
     @PutMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_SERVICE', 'ROLE_ADMIN')")
     private ResponseEntity<ResolutionDto> updateResolution(@RequestBody @Valid ResolutionDto request) {
         return ResponseEntity.ok(resolutionService.updateResolution(request));
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_SERVICE', 'ROLE_ADMIN')")
     private ResponseEntity<List<ResolutionDto>> getAllResolutions() {
         return ResponseEntity.ok(resolutionService.getAllResolutions());
     }
