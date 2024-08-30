28623d9c287f702cfb12b0155ac96aae829b58ff
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:31:14 +0200

    Add AccessDeniedException to user-service
    Introduced AccessDeniedException to handle specific cases of access denials within the user service module. This custom exception extends RuntimeException and provides a more precise way to handle authorization issues.

---
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/exceptions/AccessDeniedException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: AccessDeniedException.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.user.exceptions;
+
+public class AccessDeniedException extends RuntimeException{
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
