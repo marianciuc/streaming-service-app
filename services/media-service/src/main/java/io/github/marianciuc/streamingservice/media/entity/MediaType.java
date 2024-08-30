6c165ea8bedf64b74ea66d7d141371cb988ad8d2
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:38:07 +0200

    Add MediaType enum to media entity.
    This new enumeration defines constants for IMAGE, MOVIE, and EPISODE, providing a standardized way to manage different media types within the service. It enhances the type safety and readability of the media-related code.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/entity/MediaType.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: MediaType.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.entity;
+
+public enum MediaType {
+    IMAGE,
+    MOVIE,
+    EPISODE
+}
