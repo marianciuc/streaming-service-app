7c9d995aee2b5c392fa3a96cff2f00256187e0b8
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 21:21:13 +0200

    Add CompressingException class to handle compression errors
    Introduced a new CompressingException class extending RuntimeException. This custom exception will be used to signal issues encountered during media compression processes within the streaming service application.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/exceptions/CompressingException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: CompressingException.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.exceptions;
+
+public class CompressingException extends RuntimeException {
+    public CompressingException(String message) {
+        super(message);
+    }
+}
