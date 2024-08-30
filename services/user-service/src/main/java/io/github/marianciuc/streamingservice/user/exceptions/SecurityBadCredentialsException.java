f5ed586021ef0d39d37620e4ed14b1b5bd8b1e59
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:34:09 +0200

    Refactor SecurityBadCredentialsException constructor
    Remove dependency on BadCredentialsException from SecurityBadCredentialsException. This simplifies the custom exception and reduces coupling with Spring Security.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/exceptions/SecurityBadCredentialsException.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/exceptions/SecurityBadCredentialsException.java
@@ -1,13 +1,11 @@
 package io.github.marianciuc.streamingservice.user.exceptions;
 
-import org.springframework.security.authentication.BadCredentialsException;
-
 /**
  * The SecurityBadCredentialsException class is a custom RuntimeException that is thrown when
  * the provided credentials for user authentication are invalid.
  */
 public class SecurityBadCredentialsException extends RuntimeException {
-    public SecurityBadCredentialsException(String message, BadCredentialsException e) {
-        super(message, e);
+    public SecurityBadCredentialsException(String message) {
+        super(message);
     }
 }
