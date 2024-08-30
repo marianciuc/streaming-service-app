63dc42211c6ee027c4260d1576f1ac284cf23872
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:33:05 +0200

    Refactor JWT configuration service classes
    Updated JWT-related services to use their implementation classes and added serviceName property to enhance configuration. Changed JsonWebTokenFilter to include UserService for improved security.

--- services/order-service/src/main/java/io/github/marianciuc/streamingservice/order/config/JwtConfig.java
+++ services/order-service/src/main/java/io/github/marianciuc/streamingservice/order/config/JwtConfig.java
@@ -1,8 +1,10 @@
 package io.github.marianciuc.streamingservice.order.config;
 
-import io.github.marianciuc.jwtsecurity.service.JsonWebTokenFilter;
+import io.github.marianciuc.jwtsecurity.filters.JsonWebTokenFilter;
 import io.github.marianciuc.jwtsecurity.service.JsonWebTokenService;
 import io.github.marianciuc.jwtsecurity.service.UserService;
+import io.github.marianciuc.jwtsecurity.service.impl.JsonWebTokenServiceImpl;
+import io.github.marianciuc.jwtsecurity.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,20 +21,21 @@ public class JwtConfig {
     @Value("${security.jwt.token.secret-key}")
     private String secretKey;
 
-
+    @Value("${spring.application.name}")
+    private String serviceName;
 
     @Bean
     public JsonWebTokenService jsonWebTokenService(){
-        return new JsonWebTokenService(secretKey, accessExpiration, refreshExpiration);
+        return new JsonWebTokenServiceImpl(serviceName, secretKey, accessExpiration, refreshExpiration);
     }
 
     @Bean
-    public JsonWebTokenFilter jsonWebTokenFilter(JsonWebTokenService jsonWebTokenService){
-        return new JsonWebTokenFilter(jsonWebTokenService);
+    public JsonWebTokenFilter jsonWebTokenFilter(JsonWebTokenService jsonWebTokenService, UserService userService){
+        return new JsonWebTokenFilter(jsonWebTokenService, userService);
     }
 
     @Bean
     UserService userService(){
-        return new UserService();
+        return new UserServiceImpl();
     }
 }
