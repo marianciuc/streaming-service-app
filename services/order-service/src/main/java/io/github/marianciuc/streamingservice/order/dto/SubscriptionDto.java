0c37cb656b545e013aa81a91708d4b8d55673367
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:34:27 +0200

    Rename and refactor Subscription to SubscriptionDto
    Renamed the Subscription class to SubscriptionDto to better reflect its purpose as a Data Transfer Object (DTO). This change improves code clarity and consistency. Temporarily commented out the resolutions field for future implementation.

--- services/order-service/src/main/java/io/github/marianciuc/streamingservice/order/dto/SubscriptionDto.java
+++ services/order-service/src/main/java/io/github/marianciuc/streamingservice/order/dto/SubscriptionDto.java
@@ -5,14 +5,16 @@ import io.github.marianciuc.streamingservice.order.entity.RecordStatus;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
-public record Subscription(
+public record SubscriptionDto(
         UUID id,
         String name,
         String description,
         Integer allowedActiveSessions,
         Integer durationInDays,
+//        Set<ResolutionDto> resolutions,
         BigDecimal price,
         Boolean isTemporary,
         UUID nextSubscriptionId,
