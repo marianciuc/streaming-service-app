d5ca99747e86cccc793141323b777d32fb2abef9
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:47:35 +0200

    Refactor FFmpegCompressingService for clarity and logging
    Added Lombok's @Slf4j for logging capabilities and refactored methods for improved readability and maintainability. Extracted createTempFile and deleteFile methods to reduce redundancy and streamline the main compression logic.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegCompressingService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegCompressingService.java
@@ -11,6 +11,7 @@ package io.github.marianciuc.streamingservice.media.services.impl;
 import io.github.marianciuc.streamingservice.media.entity.Resolution;
 import io.github.marianciuc.streamingservice.media.services.VideoCompressingService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,10 +19,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FFmpegCompressingService implements VideoCompressingService {
@@ -31,61 +31,66 @@ public class FFmpegCompressingService implements VideoCompressingService {
     private static final String COMPRESSION_ERROR_MSG = "Error occurred during compression.";
     private static final String FFMPEG_COMMAND = "ffmpeg";
     private static final String CODEC = "libaom-av1";
+    private static final String TEMP_FILE_SUFFIX = ".tmp";
 
     @Override
     public byte[] compressVideo(MultipartFile file, Resolution resolution) {
         File inputFile = null;
         File outputFile = null;
-
         try {
-            inputFile = File.createTempFile("input" + file.getName(), ".tmp");
+            inputFile = createTempFile("input", file);
             file.transferTo(inputFile);
 
-            outputFile = File.createTempFile("output" + file.getName(), ".tmp");
-            return this.exactCompress(inputFile.getAbsolutePath(), outputFile.getAbsolutePath(), resolution.getHeight(), resolution.getBitrate());
+            outputFile = createTempFile("output", file);
+            return executeCompression(inputFile.getAbsolutePath(), outputFile.getAbsolutePath(), resolution);
         } catch (IOException e) {
             throw new RuntimeException(IO_EXCEPTION_MSG, e);
         } finally {
-            Objects.requireNonNull(inputFile).delete();
-            Objects.requireNonNull(outputFile).delete();
+            deleteFile(inputFile);
+            deleteFile(outputFile);
         }
     }
 
-    private byte[] exactCompress(String inputFile, String outputFile, Integer scale, Integer bitrate) {
+    private File createTempFile(String prefix, MultipartFile file) throws IOException {
+        return File.createTempFile(prefix + file.getName(), TEMP_FILE_SUFFIX);
+    }
+
+    private byte[] executeCompression(String inputFilePath, String outputFilePath, Resolution resolution) {
+        Process process = null;
         try {
-            List<String> command = createCommand(inputFile, outputFile, scale, bitrate);
-            Process process = startProcess(command);
-            int exitVal = process.waitFor();
-            if (exitVal == 0) {
-                return readFileAsBytes(outputFile);
+            List<String> command = createCommand(inputFilePath, outputFilePath, resolution.getHeight(), resolution.getBitrate());
+            process = startProcess(command);
+            if (process.waitFor() == 0) {
+                return readFileAsBytes(outputFilePath);
             }
             throw new RuntimeException(COMPRESSION_ERROR_MSG);
-        } catch (IOException | InterruptedException exception) {
-            throw new RuntimeException(INTERRUPTED_EXCEPTION_MSG, exception);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(INTERRUPTED_EXCEPTION_MSG, e);
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
         }
+    }
+
+    private List<String> createCommand(String inputFile, String outputFile, int scale, int bitrate) {
+        return List.of(
+                FFMPEG_COMMAND, "-i", inputFile, "-vf", "scale=-1:" + scale,
+                "-vcodec", CODEC, "-b:v", bitrate + "k", outputFile
+        );
     }
 
     private Process startProcess(List<String> command) throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
-        return processBuilder.start();
+        return new ProcessBuilder(command).start();
     }
 
     private byte[] readFileAsBytes(String path) throws IOException {
         return Files.readAllBytes(Paths.get(path));
     }
 
-    private List<String> createCommand(String inputFile, String outputFile, Integer scale, Integer bitrate) {
-        List<String> command = new ArrayList<>();
-        command.add(FFMPEG_COMMAND);
-        command.add("-i");
-        command.add(inputFile);
-        command.add("-vf");
-        command.add("scale=-1:" + scale);
-        command.add("-vcodec");
-        command.add(CODEC);
-        command.add("-b:v");
-        command.add(bitrate + "k");
-        command.add(outputFile);
-        return command;
+    private void deleteFile(File file) {
+        if (file != null) {
+            file.delete();
+        }
     }
 }
