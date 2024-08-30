a0f39a8832a0175116276f1f5c1a70cf4dbe83bd
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:30:49 +0200

    Remove unused DTOs, enums, and media entities
    Deleted CreateOrderMessage, UserDetailsRequest, and UserType due to redundancy. Also removed Video entity and its repository as they are no longer in use. Cleans up the codebase and improves maintainability.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/repository/VideoRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.marianciuc.streamingservice.media.repository;
-
-import io.github.marianciuc.streamingservice.media.entity.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface VideoRepository extends JpaRepository<Video, UUID> {
-}
