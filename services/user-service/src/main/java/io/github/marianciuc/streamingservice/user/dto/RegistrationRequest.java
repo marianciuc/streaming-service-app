9c8eb9fcb9e6e0fc78d0e18738d9d20ed8e9d2b0
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:37:56 +0200

    Add 'role' field to RegistrationRequest record
    This change introduces a new 'role' field to the RegistrationRequest record to accommodate user role assignments. The new field is validated to ensure it's not blank, thereby enhancing data consistency and security.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/dto/RegistrationRequest.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/dto/RegistrationRequest.java
@@ -1,16 +1,16 @@
 package io.github.marianciuc.streamingservice.user.dto;
 
+import io.github.marianciuc.streamingservice.user.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+/**
+ * Record class to represent the RegistrationRequest.
+ */
 public record RegistrationRequest(
-        @NotBlank(message = "Username is mandatory")
-        String username,
-
-        @Email(message = "Email should be valid")
-        String email,
-
-        @NotBlank(message = "Password is mandatory")
-        String password
+        @NotBlank(message = "Username is mandatory") String username,
+        @Email(message = "Email should be valid") String email,
+        @NotBlank(message = "Password is mandatory") String password,
+        @NotBlank Role role
 ) {
 }
