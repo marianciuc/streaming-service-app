ba0e5b404cf1f3b293689c3f66d3311400e8c5ae
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 17:07:11 +0200

    Add method to find all media by content ID
    Introduce a new method in MediaRepository to retrieve a list of media entities based on a given content ID. This enhances the repository functionality, allowing more flexible media queries.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/repository/MediaRepository.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/repository/MediaRepository.java
@@ -4,9 +4,12 @@ import io.github.marianciuc.streamingservice.media.entity.Media;
 import io.github.marianciuc.streamingservice.media.entity.MediaType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface MediaRepository extends JpaRepository<Media, UUID> {
     Optional<Media> findByIdAndMediaType(UUID id, MediaType mediaType);
+
+    List<Media> findAllByContentId(UUID contentId);
 }
