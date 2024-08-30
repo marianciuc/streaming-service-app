c3b14f5fc2d1f2b87ea4e3a6ba3bc5afdd54b21e
Vladimir Marianciuc <boosth14@gmail.com>
2024-08-30 16:35:30 +0200

    Add JWT configuration support
    Introduce a new JwtConfig class to configure JSON Web Token settings for access and refresh tokens. This configuration includes beans for JWT services and filters, essential for token authentication in the media service.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/config/JwtConfig.java
@@ -0,0 +1,48 @@
+package io.github.marianciuc.streamingservice.media.config;
+
+import io.github.marianciuc.jwtsecurity.filters.JsonWebTokenFilter;
+import io.github.marianciuc.jwtsecurity.service.JsonWebTokenService;
+import io.github.marianciuc.jwtsecurity.service.UserService;
+import io.github.marianciuc.jwtsecurity.service.impl.JsonWebTokenServiceImpl;
+import io.github.marianciuc.jwtsecurity.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is a configuration class for JWT (JSON Web Token).
+ * It includes the settings for access and refresh tokens and provides Beans for JWT services and filters.
+ */
+@Configuration
+public class JwtConfig {
+
+    /** Expiration time for an access token. */
+    @Value("${security.jwt.token.accessExpiration}")
+    private Long accessExpiration;
+
+    /** Expiration time for a refresh token. */
+    @Value("${security.jwt.token.refreshToken}")
+    private Long refreshExpiration;
+
+    /** Secret key for token generation and verification. */
+    @Value("${security.jwt.token.secret-key}")
+    private String secretKey;
+
+    @Value("${spring.application.name}")
+    private String serviceName;
+
+    @Bean
+    public JsonWebTokenService jsonWebTokenService(){
+        return new JsonWebTokenServiceImpl(serviceName, secretKey, accessExpiration, refreshExpiration);
+    }
+
+    @Bean
+    public JsonWebTokenFilter jsonWebTokenFilter(JsonWebTokenService jsonWebTokenService, UserService userService){
+        return new JsonWebTokenFilter(jsonWebTokenService, userService);
+    }
+
+    @Bean
+    public UserService userService(){
+        return new UserServiceImpl();
+    }
+}
