f5067bb6419eacaa21f4aba163a77775262b14c9
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-02 23:16:53 +0200

    Add JavaDoc comments to ImageService interface
    Enhance the ImageService interface with JavaDoc comments for better code documentation. This includes detailed descriptions of methods and their parameters, improving code readability and maintainability.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/ImageService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/ImageService.java
@@ -14,12 +14,32 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
+/**
+ * Service interface for managing images.
+ */
 public interface ImageService {
 
+    /**
+     * Retrieves an image by its ID.
+     *
+     * @param id The UUID of the image.
+     * @return An ImageDto containing the image data.
+     */
     ImageDto getImage(UUID id);
 
+    /**
+     * Uploads a new image.
+     *
+     * @param file The image file to upload.
+     * @param authentication The authentication information of the user uploading the image.
+     * @return The UUID of the uploaded image.
+     */
     UUID uploadImage(MultipartFile file, Authentication authentication);
 
+    /**
+     * Deletes an image by its ID.
+     *
+     * @param id The UUID of the image to delete.
+     */
     void deleteImage(UUID id);
-
 }
