ade4820633174da0537730c16e509005d7a11132
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-02 18:40:03 +0200

    Add Minio integration for video uploads
    Introduce MinioConfig and MinioService classes to integrate Minio for video file uploads. MinioConfig sets up the MinioClient bean while MinioService handles the actual upload and bucket creation logic.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/config/MinioConfig.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: MinioConfig.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.config;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+    @Value("${minio.url}")
+    private String minioUrl;
+
+    @Value("${minio.access-key}")
+    private String accessKey;
+
+    @Value("${minio.secret-key}")
+    private String secretKey;
+
+    @Bean
+    public MinioClient minioClient() {
+        return MinioClient.builder()
+                .endpoint(minioUrl)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
