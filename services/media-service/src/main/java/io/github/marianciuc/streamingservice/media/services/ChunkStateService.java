5b1d90edbb6c55da930287e15a266c2cbcc7739d
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-02 18:38:48 +0200

    Add ChunkStateService interface to manage chunk upload states
    This new interface includes methods for updating, retrieving, and deleting the upload status of file chunks. It will be used to track the progress of file uploads by chunk.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/ChunkStateService.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: ChunkStateService.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.services;
+
+import java.util.UUID;
+
+public interface ChunkStateService {
+    void updateChunkUploadStatus(UUID fileId, int chunkNumber, int totalChunks);
+    Boolean[] getChunkUploadStatus(UUID fileId);
+    void deleteChunkUploadStatus(UUID fileId);
+}
