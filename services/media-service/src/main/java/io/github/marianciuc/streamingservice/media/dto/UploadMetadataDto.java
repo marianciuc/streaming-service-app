fd46bdfb408b7d6a237b30d919b7f9a6c31f8b03
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-02 18:39:42 +0200

    Add UploadMetadataDto class for media upload metadata
    This new class encapsulates the metadata required for media uploads, including a temporary file ID and the total number of chunks. It enhances the structure and management of media upload processes.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/dto/UploadMetadataDto.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: MetadataDto.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.dto;
+
+import java.util.UUID;
+
+public record UploadMetadataDto(
+        UUID temporaryFileId,
+        int totalChunks
+) {
+}
