fc57a930fd89bc2ebfb0498d5aff197282133bd7
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-02 23:17:28 +0200

    Add custom VideoUploadException class
    Introduced a new VideoUploadException class extending RuntimeException to handle specific video upload errors. This ensures better error handling and more informative messaging during video upload failures.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/exceptions/VideoUploadException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: PhotoUploadException.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.exceptions;
+
+public class VideoUploadException extends RuntimeException {
+    public VideoUploadException(String message) {
+        super(message);
+    }
+}
