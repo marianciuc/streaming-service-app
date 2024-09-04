97fc5f43d43bffc89a23bdba99878383d952cbce
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:50:36 +0200

    Rename and update video compression method with exception handling
    Renamed the compressVideo method to compressVideoAndUploadToStorage to better reflect its functionality. Added detailed Javadoc comments and exception handling to ensure robustness during the video compression process.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/VideoCompressingService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/VideoCompressingService.java
@@ -9,10 +9,23 @@
 package io.github.marianciuc.streamingservice.media.services;
 
 import io.github.marianciuc.streamingservice.media.dto.ResolutionDto;
+import io.github.marianciuc.streamingservice.media.exceptions.CompressingException;
 
 import java.io.InputStream;
+import java.util.UUID;
 
+/**
+ * Interface representing a video compression service.
+ */
 public interface VideoCompressingService {
-    byte [] compressVideo(InputStream file, ResolutionDto resolution);
-
+    /**
+     * Compresses the input video stream to the specified resolution and uploads it to the storage.
+     *
+     * @param io the input stream of the video to be compressed
+     * @param resolution the target resolution for the video compression
+     * @param id the unique identifier of the video
+     * @return the storage location of the uploaded compressed video
+     * @throws CompressingException if an error occurs during the video compression process
+     */
+    String compressVideoAndUploadToStorage(InputStream io, ResolutionDto resolution, UUID id) throws CompressingException;
 }
