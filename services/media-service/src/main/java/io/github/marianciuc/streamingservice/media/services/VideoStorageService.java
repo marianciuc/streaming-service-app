5944a684bc46293f01acc837782a01c4994195b7
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:49:56 +0200

    Refactor VideoStorageService for improved upload handling
    Add constants for content and chunk paths, and a new uploadFile method to handle file uploads. This allows more precise error handling with specific exceptions like MinioException, IOException, NoSuchAlgorithmException, and InvalidKeyException.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/VideoStorageService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/VideoStorageService.java
@@ -8,12 +8,21 @@
 
 package io.github.marianciuc.streamingservice.media.services;
 
+import io.minio.errors.MinioException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 
 public interface VideoStorageService {
+
+    String CONTENT_PATH = "videos/";
+    String CHUNKS_PATH = "chunks/";
+
+    void uploadFile(String objectName, InputStream inputStream, long size, String contentType) throws MinioException, IOException, NoSuchAlgorithmException, InvalidKeyException;
     void uploadVideoChunk(MultipartFile chunk, Integer chunkNumber, UUID fileId);
     void deleteVideo(UUID fileId);
     InputStream assembleVideo(UUID fileId);
