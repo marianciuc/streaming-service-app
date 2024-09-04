6985c4fd81cae01218fcd22e3e9262809393fd4c
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 23:57:00 +0200

    Add new Kafka listener for video conversion tasks
    Introduced a new Kafka listener to handle video conversion tasks using the `ConvertingTaskDto`. Now `TaskConsumer` processes both delete and conversion tasks by interacting with `VideoService`.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/TaskConsumer.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/TaskConsumer.java
@@ -8,7 +8,9 @@
 
 package io.github.marianciuc.streamingservice.media.kafka;
 
+import io.github.marianciuc.streamingservice.media.dto.ConvertingTaskDto;
 import io.github.marianciuc.streamingservice.media.dto.MediaDeleteMessage;
+import io.github.marianciuc.streamingservice.media.services.VideoService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +18,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class TaskConsumer {
 
+    private VideoService videoService;
+
     @KafkaListener(topics = "video-converting-topic", groupId = "${spring.kafka.consumer.group-id}")
     private void consumeCompressTask(MediaDeleteMessage mediaDto) {
         videoService.deleteVideoByContent(mediaDto.contentId());
+    }
+
+    @KafkaListener(topics = "video-converting-topic", groupId = "${spring.kafka.consumer.group-id}")
+    private void consumeCompressTask(ConvertingTaskDto convertingTaskDto) {
+        videoService.processMediaFile(convertingTaskDto);
     }
 }
