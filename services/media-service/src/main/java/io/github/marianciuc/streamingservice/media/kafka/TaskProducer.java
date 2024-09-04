f7158ae359b654d53e6b1bcba6e788f0608b3b5d
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:50:41 +0200

    Refactor TaskProducer to simplify video conversion task
    Updated the TaskProducer to reduce complexity by removing unnecessary services and dependencies. Now, it directly sends a ConvertingTaskDto to the Kafka topic, streamlining the video conversion process.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/TaskProducer.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/TaskProducer.java
@@ -8,42 +8,22 @@
 
 package io.github.marianciuc.streamingservice.media.kafka;
 
+import io.github.marianciuc.streamingservice.media.dto.ConvertingTaskDto;
 import io.github.marianciuc.streamingservice.media.dto.ResolutionDto;
-import io.github.marianciuc.streamingservice.media.entity.Resolution;
-import io.github.marianciuc.streamingservice.media.services.ResolutionService;
-import io.github.marianciuc.streamingservice.media.services.VideoCompressingService;
-import io.github.marianciuc.streamingservice.media.services.VideoStorageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class TaskProducer {
 
-    private final KafkaTemplate<String, UUID> kafkaTemplate;
-    private final VideoStorageService videoStorageService;
-    private final VideoCompressingService videoCompressingService;
-    private final ResolutionService resolutionService;
+    private final KafkaTemplate<String, ConvertingTaskDto> kafkaTemplate;
 
-    public void sendTaskToQueue(UUID fileId, int chunkNumber, int totalChunks, UUID contentId, UUID sourceResolutionId) {
-        // поставить статус фильм в обработке
-        try (InputStream is = videoStorageService.assembleVideo(fileId)) {
-            List<ResolutionDto> resolutions = resolutionService.getAllResolutions().stream().filter((resolutionDto -> resolutionDto.height() < sourceResolution.height())).toList();
-
-            for (ResolutionDto resolution : resolutions) {
-                videoCompressingService.processVideo(is, resolution);
-            }
-            String masterPlaylistPath = videoStorageService.createMasterPlaylist(fileId, resolutions);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    public void sendTaskToQueue(UUID fileId, ResolutionDto resolutionDto) {
+        kafkaTemplate.send("video-converting-topic",
+                new ConvertingTaskDto(fileId, resolutionDto));
     }
-
-
 }
