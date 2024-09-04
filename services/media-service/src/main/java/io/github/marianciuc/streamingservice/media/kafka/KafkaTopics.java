0d901226f90a865e26023a1117189921ce69c5fe
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 00:36:46 +0200

    Add START_COMPRESSING_TOPIC constant to KafkaTopics
    Introduce a new constant `START_COMPRESSING_TOPIC` to handle messages related to video conversion. This prepares the system for future enhancements involving video processing workflows.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/KafkaTopics.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/KafkaTopics.java
@@ -7,6 +7,7 @@ public class KafkaTopics {
     public final static String RESOLUTION_CREATED_TOPIC = "resolution-created-topic";
     public final static String RESOLUTION_UPDATED_TOPIC = "resolution-updated-topic";
     public final static String RESOLUTION_DELETED_TOPIC = "resolution-deleted-topic";
+    public final static String START_COMPRESSING_TOPIC = "video-converting-topic";
     public final static String IMAGE_DELETED_TOPIC = "image-deleted-topic";
     public final static String VIDEO_DELETED_TOPIC = "video-deleted-topic";
     public final static String VIDEO_UPLOADED_TOPIC = "video-uploaded-topic";
