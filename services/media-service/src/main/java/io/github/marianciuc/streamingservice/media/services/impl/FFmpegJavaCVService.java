8738468e9b68fc6a07218a739154174c2e1038b2
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:49:22 +0200

    Refactor video compression to support HLS and storage upload
    Updated the FFmpegJavaCVService to compress video into HLS format and upload chunks and playlist to storage. Introduced VideoStorageService dependencies and added methods to handle chunked uploads and playlist generation. Removed deprecated methods and streamlined the compression process.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegJavaCVService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegJavaCVService.java
@@ -5,30 +5,33 @@
  * File: FFmpegJavaCVService.java
  *
  */
-
 package io.github.marianciuc.streamingservice.media.services.impl;
 
 import io.github.marianciuc.streamingservice.media.dto.ResolutionDto;
 import io.github.marianciuc.streamingservice.media.exceptions.CompressingException;
 import io.github.marianciuc.streamingservice.media.services.VideoCompressingService;
+import io.github.marianciuc.streamingservice.media.services.VideoStorageService;
+import io.minio.errors.MinioException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.ffmpeg.global.avcodec;
 import org.bytedeco.ffmpeg.global.avutil;
-import org.bytedeco.javacv.*;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FFmpegFrameRecorder;
+import org.bytedeco.javacv.Frame;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
-/**
- * Service that handles video compression using FFmpeg and JavaCV.
- * Implements the VideoCompressingService interface.
- */
+import static io.github.marianciuc.streamingservice.media.services.VideoStorageService.CHUNKS_PATH;
+import static io.github.marianciuc.streamingservice.media.services.VideoStorageService.CONTENT_PATH;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -38,89 +41,122 @@ public class FFmpegJavaCVService implements VideoCompressingService {
     private static final String IO_EXCEPTION_MSG = "An exception occurred while interacting with the filesystem.";
     private static final String FRAME_GRABBING_ERROR_MSG = "Error during frame grabbing or recording.";
     private static final String PREFIX_INPUT_FILE = "input";
-    private static final String PREFIX_OUTPUT_FILE = "output";
-
     private static final int VIDEO_CODEC = avcodec.AV_CODEC_ID_H264;
     private static final int AUDIO_CODEC = avcodec.AV_CODEC_ID_AAC;
     private static final int PIXEL_FORMAT = avutil.AV_PIX_FMT_YUV420P;
-    private static final String FORMAT = "mp4";
+    private static final String OUTPUT_FORMAT = "hls";
+    private static final int CHUNK_DURATION_IN_SECONDS = 10;
+    private static final String HLS_SEGMENT_FILENAME = "segment%d.ts";
 
-    /**
-     * Compresses the given video file to the specified resolution.
-     *
-     * @param file       the video file to be compressed
-     * @param resolution the desired resolution to which the video should be compressed
-     * @return a byte array containing the compressed video
-     */
+    private static final String CONTENT_TYPE_VIDEO_MP2T = "video/mp2t";
+    private static final String CONTENT_TYPE_APPLICATION_X_MPEGURL = "application/x-mpegURL";
+    private static final String PATH_CHUNK_TEMPLATE = CONTENT_PATH + "%d/ " + CHUNKS_PATH + "%s/segment%d.ts";
+    private static final String PATH_PLAYLIST_TEMPLATE = CONTENT_PATH + "hls/%s/index.m3u8";
+
+    private final VideoStorageService videoStorageService;
+
     @Override
-    public byte[] compressVideo(InputStream io, ResolutionDto resolution) {
+    public String compressVideoAndUploadToStorage(InputStream io, ResolutionDto resolution, UUID id) throws CompressingException {
         File inputFile = null;
-        File outputFile = null;
+
         try {
             inputFile = createTempFile(PREFIX_INPUT_FILE, "input-temp-file");
             Files.copy(io, Path.of(inputFile.getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
-
-            outputFile = createTempFile(PREFIX_OUTPUT_FILE, "output-temp-file");
-            executeCompression(inputFile.getAbsolutePath(), outputFile.getAbsolutePath(), resolution);
-            return Files.readAllBytes(outputFile.toPath());
+            return executeCompression(inputFile.getAbsolutePath(), resolution, id);
         } catch (IOException e) {
-            throw new RuntimeException(IO_EXCEPTION_MSG, e);
+            throw new CompressingException(IO_EXCEPTION_MSG);
         } finally {
             deleteFile(inputFile);
-            deleteFile(outputFile);
         }
     }
 
-
-    /**
-     * Executes the video compression process using the specified input file path,
-     * output file path, and target resolution.
-     *
-     * @param inputFilePath  the path to the input video file to be compressed
-     * @param outputFilePath the path where the compressed video file will be saved
-     * @param resolution     the target resolution to which the video should be compressed
-     */
-    private void executeCompression(String inputFilePath, String outputFilePath, ResolutionDto resolution) {
+    private String executeCompression(String inputFilePath, ResolutionDto resolution, UUID id) {
         log.info("Starting video compression...");
         log.info("Input file: {}", inputFilePath);
-        log.info("Output file: {}", outputFilePath);
-
-        FFmpegLogCallback.set();
 
         try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFilePath);
-             FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(
-                     outputFilePath,
-                     resolution.width(),
-                     resolution.height()
-             )
-        ) {
-            grabber.setFormat(FORMAT);
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
             grabber.start();
+            int frameRate = (int) grabber.getFrameRate();
+            int totalFramesPerChunk = frameRate * CHUNK_DURATION_IN_SECONDS;
 
-            recorder.setVideoCodec(VIDEO_CODEC);
-            recorder.setPixelFormat(PIXEL_FORMAT);
-            recorder.setAudioCodec(AUDIO_CODEC);
-            recorder.setSampleRate(grabber.getSampleRate());
-            recorder.setAudioChannels(grabber.getAudioChannels());
-            recorder.setFormat(FORMAT);
-            recorder.setFrameRate(grabber.getFrameRate());
-            recorder.setVideoBitrate(resolution.bitrate());
-            recorder.start();
+            Frame frame;
+            FFmpegFrameRecorder recorder = null;
+            int chunkCount = 0;
+            StringBuilder playlistContent = new StringBuilder("#EXTM3U\n");
 
-            while (true) {
-                Frame frame = grabber.grab();
-                if (frame == null) {
-                    break;
+            while ((frame = grabber.grab()) != null) {
+                if (recorder == null || recorder.getFrameNumber() >= totalFramesPerChunk) {
+                    if (recorder != null) {
+                        recorder.stop();
+                        uploadChunkAndResetStream(outputStream, resolution, id, playlistContent, chunkCount);
+                        chunkCount++;
+                    }
+
+                    recorder = initializeRecorder(outputStream, resolution, frameRate, grabber);
                 }
+
                 recorder.record(frame);
             }
 
-            recorder.stop();
+            if (recorder != null) {
+                recorder.stop();
+                uploadChunkAndResetStream(outputStream, resolution, id, playlistContent, chunkCount);
+            }
+
             grabber.stop();
-        } catch (FrameGrabber.Exception | FrameRecorder.Exception e) {
+            return savePlaylist(id, playlistContent.toString(), resolution.height());
+        } catch (Exception e) {
             log.error(FRAME_GRABBING_ERROR_MSG, e);
             throw new CompressingException(FRAME_GRABBING_ERROR_MSG);
         }
+    }
+
+    private FFmpegFrameRecorder initializeRecorder(ByteArrayOutputStream outputStream, ResolutionDto resolution, int frameRate, FFmpegFrameGrabber grabber) throws Exception {
+        FFmpegFrameRecorder recorder = new FFmpegFrameRecorder(outputStream, resolution.width(), resolution.height());
+
+        setupRecorder(recorder, frameRate, resolution.bitrate(), grabber);
+        recorder.start();
+
+        return recorder;
+    }
+
+    private void setupRecorder(FFmpegFrameRecorder recorder, int frameRate, int bitrate, FFmpegFrameGrabber grabber) {
+        recorder.setVideoCodec(VIDEO_CODEC);
+        recorder.setAudioCodec(AUDIO_CODEC);
+        recorder.setPixelFormat(PIXEL_FORMAT);
+        recorder.setFormat(OUTPUT_FORMAT);
+        recorder.setFrameRate(frameRate);
+        recorder.setSampleRate(grabber.getSampleRate());
+        recorder.setVideoBitrate(bitrate);
+        recorder.setOption("hls_list_size", "0");
+        recorder.setOption("hls_time", String.valueOf(CHUNK_DURATION_IN_SECONDS));
+        recorder.setOption("hls_segment_filename", HLS_SEGMENT_FILENAME);
+    }
+
+    private void uploadChunkAndResetStream(ByteArrayOutputStream outputStream, ResolutionDto resolution, UUID fileId, StringBuilder playlistContent, int chunkCount) throws IOException, MinioException, NoSuchAlgorithmException, InvalidKeyException {
+        String objectName = String.format(PATH_CHUNK_TEMPLATE, resolution.height(), fileId, chunkCount);
+
+        try (InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray())) {
+            videoStorageService.uploadFile(objectName, inputStream, outputStream.size(), CONTENT_TYPE_VIDEO_MP2T);
+
+            playlistContent.append("#EXTINF:").append(CHUNK_DURATION_IN_SECONDS).append(",\n")
+                    .append(objectName)
+                    .append("\n");
+        }
+
+        outputStream.reset();
+    }
+
+    private String savePlaylist(UUID id, String playlistContent, int resolutionHeight) throws MinioException, IOException, NoSuchAlgorithmException, InvalidKeyException {
+        String objectName = String.format(PATH_PLAYLIST_TEMPLATE, id + "_" + resolutionHeight);
+
+        try (InputStream playlistStream = new ByteArrayInputStream(playlistContent.getBytes())) {
+            videoStorageService.uploadFile(objectName, playlistStream, playlistContent.length(), CONTENT_TYPE_APPLICATION_X_MPEGURL);
+        }
+
+        return objectName;
     }
 
     private File createTempFile(String prefix, String fileName) throws IOException {
