3d58784ad667e803cbdd7b79e7126ce7f59ac840
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 00:35:51 +0200

    Add javadocs to VideoUploadStatusService interface
    Provide documentation for managing video upload statuses in the VideoUploadStatusService interface. This includes details on the `createVideoUploadStatus` method and its parameters.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/VideoUploadStatusService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/VideoUploadStatusService.java
@@ -12,6 +12,19 @@ import io.github.marianciuc.streamingservice.media.enums.StatusType;
 
 import java.util.UUID;
 
+/**
+ * Interface for managing the upload statuses of videos.
+ * This service provides methods to create the upload status of a video.
+ */
 public interface VideoUploadStatusService {
+
+    /**
+     * Creates a video upload status entry.
+     *
+     * @param videoId the unique identifier of the video
+     * @param message the status message associated with the video upload
+     * @param statusType the type of status, which can be INFO, WARNING, or ERROR
+     * @param title the title of the status entry
+     */
     void createVideoUploadStatus(UUID videoId, String message, StatusType statusType, String title);
 }
