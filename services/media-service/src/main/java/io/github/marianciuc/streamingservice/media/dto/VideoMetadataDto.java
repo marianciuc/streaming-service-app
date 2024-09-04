32776bbec17e1c3e2732e7371dd8aa7c0b51dbf0
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:50:17 +0200

    Remove unused fields from VideoMetadataDto
    Deleted 'resolution' and 'mediaType' fields as they are no longer utilized in the current implementation. This change simplifies the data transfer object and reduces unnecessary complexity.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/dto/VideoMetadataDto.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/dto/VideoMetadataDto.java
@@ -14,8 +14,6 @@ import java.util.UUID;
 
 public record VideoMetadataDto (
         UUID id,
-        ResolutionMessage resolution,
-        UUID contentId,
-        MediaType mediaType
+        UUID contentId
 ){
 }
