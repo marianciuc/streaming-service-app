def37c0c252abd8f3b1ac4864cbadddee392375d
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:50:04 +0200

    Add VideoStatues enum to media-service
    Introduce an enum representing various states of video processing. This provides a structured way to manage and reference video statuses throughout the application.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/enums/VideoStatues.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: VideoStatues.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.enums;
+
+public enum VideoStatues {
+    CREATED,
+    PREPARING,
+    PREPARED,
+    UPLOADED,
+    PROCESSING,
+    PROCESSED,
+    DELETED,
+    ERROR
+}
