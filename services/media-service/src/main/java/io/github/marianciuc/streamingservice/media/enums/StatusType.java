4a5226fb34319f14a78a6879d4b3776ad44a0051
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:49:40 +0200

    Add StatusType enum for media status handling
    This commit introduces the StatusType enum within the media service. It defines three statuses: INFO, WARNING, and ERROR, to standardize how media statuses are handled throughout the application.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/enums/StatusType.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: StatusType.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.enums;
+
+public enum StatusType {
+    INFO, WARNING, ERROR
+}
