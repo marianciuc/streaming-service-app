f87124734a14bfe58907c8d8845e92540ea907ef
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 00:35:30 +0200

    Remove unused classes related to Kafka integration
    Deleted InvalidFileType, KafkaMediaProducer, and MediaConsumer classes as they are no longer needed in the project. This cleanup reduces clutter and improves code maintainability.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/exceptions/InvalidFileType.java
+++
@@ -1,7 +0,0 @@
-package io.github.marianciuc.streamingservice.media.exceptions;
-
-public class InvalidFileType extends RuntimeException {
-    public InvalidFileType(String message) {
-        super(message);
-    }
-}
