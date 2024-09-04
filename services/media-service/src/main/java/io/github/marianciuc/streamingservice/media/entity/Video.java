9eb66065b14dcdcde03770c9777b3f6d2e198378
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-04 22:50:48 +0200

    Add Video entity class
    Introduce the Video entity for managing video details in the database. This class includes fields for content, user IDs, media type, and status, and it has relationships with VideoFileMetadata and VideoUploadingStatus entities.

---
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/entity/Video.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: Video.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.media.entity;
+
+import io.github.marianciuc.streamingservice.media.enums.MediaType;
+import io.github.marianciuc.streamingservice.media.enums.VideoStatues;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "videos")
+public class Video {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "content_id")
+    private UUID contentId;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "content_type")
+    private String contentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "media_type")
+    private MediaType mediaType;
+
+    @Column(name = "resolution_count")
+    private Integer resolutionCounter;
+
+    @Column(name = "resolutions_processed")
+    private Integer resolutionsProcessed;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private VideoStatues status;
+
+    @Column(name = "total_chunks")
+    private int totalChunks;
+
+    @Column(name = "master_playlist_path")
+    private String masterPlaylistPath;
+
+    @OneToMany(mappedBy = "videos", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<VideoFileMetadata> files;
+
+    @OneToMany(mappedBy = "videos", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<VideoUploadingStatus> statuses;
+}
