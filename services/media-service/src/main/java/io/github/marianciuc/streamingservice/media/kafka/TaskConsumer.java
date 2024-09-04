6c2bc132f89df7256538778e5b68ad53f273346f
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 00:36:15 +0200

    Refactor TaskConsumer to use final and required args constructor
    Replaced mutable `VideoService` with a final field and added `@RequiredArgsConstructor` for dependency injection. Consolidated Kafka topic constant and updated the consume task method to handle `ConvertingTaskDto` instead of `MediaDeleteMessage`.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/TaskConsumer.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/kafka/TaskConsumer.java
@@ -9,23 +9,21 @@
 package io.github.marianciuc.streamingservice.media.kafka;
 
 import io.github.marianciuc.streamingservice.media.dto.ConvertingTaskDto;
-import io.github.marianciuc.streamingservice.media.dto.MediaDeleteMessage;
 import io.github.marianciuc.streamingservice.media.services.VideoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import static io.github.marianciuc.streamingservice.media.kafka.KafkaTopics.START_COMPRESSING_TOPIC;
+
 
 @Component
+@RequiredArgsConstructor
 public class TaskConsumer {
 
-    private VideoService videoService;
+    private final VideoService videoService;
 
-    @KafkaListener(topics = "video-converting-topic", groupId = "${spring.kafka.consumer.group-id}")
-    private void consumeCompressTask(MediaDeleteMessage mediaDto) {
-        videoService.deleteVideoByContent(mediaDto.contentId());
-    }
-
-    @KafkaListener(topics = "video-converting-topic", groupId = "${spring.kafka.consumer.group-id}")
+    @KafkaListener(topics = START_COMPRESSING_TOPIC, groupId = "${spring.kafka.consumer.group-id}")
     private void consumeCompressTask(ConvertingTaskDto convertingTaskDto) {
         videoService.processMediaFile(convertingTaskDto);
     }
