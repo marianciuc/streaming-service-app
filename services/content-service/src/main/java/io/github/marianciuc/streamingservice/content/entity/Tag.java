6c365c91dcfbb1e58819fc5273d1a6fde61ad155
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 21:03:50 +0200

    Add Tag entity to content-service
    Introduced the Tag entity with fields and JPA annotations. This entity will manage tags associated with content, including a many-to-many relationship with the Content entity.

---
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Tag.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: Tag.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.content.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Data
+@Entity
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tags")
+public class Tag extends BaseEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private List<Content> contents;
+}
