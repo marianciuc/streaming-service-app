1e35962d5cfa2b1870fb1005bbcb34be82f7708d
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 20:36:44 +0200

    Add AccessDeniedException class to customer exceptions
    Introduce AccessDeniedException to handle unauthorized access scenarios. This new class will provide a specific exception type for better error management and clearer debugging.

---
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/exceptions/AccessDeniedException.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: AccessDeniedException.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.customer.exceptions;
+
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+    }
+}
