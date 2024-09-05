6102d5a277c5593882f4a917af6c298092e80ebc
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 19:36:54 +0200

    Enhance CustomerRepository with JpaSpecificationExecutor support
    Added JpaSpecificationExecutor to the CustomerRepository interface to enable support for complex queries and specifications. This enhancement allows for more flexible and dynamic query construction in the Customer service module.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/repository/CustomerRepository.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/repository/CustomerRepository.java
@@ -10,9 +10,10 @@ package io.github.marianciuc.streamingservice.customer.repository;
 
 import io.github.marianciuc.streamingservice.customer.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
-public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+public interface CustomerRepository extends JpaRepository<Customer, UUID>, JpaSpecificationExecutor<Customer> {
 
 }
