714abe3dea94c12e08a53cb7c1019bf04a9b7d7b
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 21:04:10 +0200

    Add masterPlaylistId to UploadVideoDto
    Incorporated a new field 'masterPlaylistId' in UploadVideoDto to support referencing the master playlist for the uploaded video. This update ensures better management and association of video content with its playlists.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/dto/UploadVideoDto.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/dto/UploadVideoDto.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 
 public record UploadVideoDto (
         UUID contentId,
-        RecordStatus recordStatus
+        RecordStatus recordStatus,
+        UUID masterPlaylistId
 ) {
 }
