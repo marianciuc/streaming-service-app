37203b80eaac827d5a151d536359dfa49ab67939
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 00:00:08 +0200

    Refactor Episode entity structure and update fields
    Renamed 'episodeNumber' to 'number' and added 'duration' and 'masterPlaylistUrl' fields. Changed 'releaseDate' type from LocalDateTime to LocalDate. Removed excessive comments to streamline the code.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Episode.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Episode.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -19,35 +20,28 @@ import java.util.UUID;
 @Table(name = "episodes")
 public class Episode extends BaseEntity{
 
-    /**
-     * The season variable represents the season to which an episode belongs.
-     */
     @ManyToOne
     @JoinColumn(name = "season_id", nullable = false)
     private Season season;
 
-    /**
-     * The episodeNumber variable represents the episode number of an episode of a TV series.
-     * It is mapped to the database column "episode_number".
-     * It is a required field and cannot be null.
-     */
     @Column(name = "episode_number", nullable = false)
-    private Integer episodeNumber;
+    private Integer number;
 
-    /**
-     * The title variable represents the title of an episode of a TV series.
-     * It is mapped to the database column "title".
-     * It is a required field and cannot be null.
-     */
+    @Column(name = "duration", nullable = false)
+    private Integer duration;
+
     @Column(name = "title", nullable = false)
     private String title;
 
     @Column(name = "release_date", nullable = false)
-    private LocalDateTime releaseDate;
+    private LocalDate releaseDate;
 
     @Column(name = "description", nullable = false)
     private String description;
 
+    @Column(name = "master_playlist_url")
+    private String masterPlaylistUrl;
+
     @Column(name = "master_playlist_id")
-    UUID masterPlaylistId;
+    private UUID masterPlaylistId;
 }
