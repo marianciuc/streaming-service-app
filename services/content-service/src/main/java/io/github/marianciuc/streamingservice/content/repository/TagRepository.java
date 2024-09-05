bbfdc1f6bcaba42e4fa2b22cb770f512a62fc970
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 23:58:03 +0200

    Add TagRepository interface for tag entity queries
    This commit introduces the TagRepository interface extending Jpa

---
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/repository/TagRepository.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: TagRepository.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.content.repository;
+
+import io.github.marianciuc.streamingservice.content.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TagRepository extends JpaRepository<Tag, UUID> {
+    List<Tag> findByNameStartingWith(String prefix);
+}
