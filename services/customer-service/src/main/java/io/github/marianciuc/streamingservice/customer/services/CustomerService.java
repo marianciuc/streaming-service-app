783783cf79c991ac31e876a2f7253022af8fba33
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 20:37:23 +0200

    Refactor CustomerService to interface
    Converted CustomerService from a concrete class to an interface to standardize customer-related operations. Methods for creating, verifying, and updating customers were converted to abstract declarations, facilitating easier implementation and testing.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/services/CustomerService.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/services/CustomerService.java
@@ -8,121 +8,71 @@
 
 package io.github.marianciuc.streamingservice.customer.services;
 
-import io.github.marianciuc.jwtsecurity.entity.JwtUser;
 import io.github.marianciuc.streamingservice.customer.dto.CreateCustomerMessage;
 import io.github.marianciuc.streamingservice.customer.dto.CustomerDto;
 import io.github.marianciuc.streamingservice.customer.dto.PaginationResponse;
-import io.github.marianciuc.streamingservice.customer.exceptions.VerificationCodeException;
-import io.github.marianciuc.streamingservice.customer.model.Customer;
-import io.github.marianciuc.streamingservice.customer.repository.CustomerRepository;
-import io.github.marianciuc.streamingservice.customer.specifications.CustomerSpecification;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.Authentication;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 /**
- * Service class that handles operations related to customers, such as creating a customer,
- * starting email verification, verifying email codes, and finding customers based on filters.
+ * Service interface for managing customer-related operations.
  */
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class CustomerService {
+public interface CustomerService {
 
-    private static final String DEFAULT_THEME = "light";
-    private static final String DEFAULT_LANGUAGE = "en";
-    private static final boolean DEFAULT_RECEIVE_NEWSLETTER = false;
-    private static final boolean DEFAULT_ENABLE_NOTIFICATIONS = true;
-    private static final boolean DEFAULT_PROFILE_IS_COMPLETED = false;
-    private static final boolean DEFAULT_EMAIL_VERIFIED = false;
-
-    private final CustomerRepository repository;
-    private final EmailVerificationService emailVerificationService;
+    /**
+     * Creates a new customer in the system.
+     *
+     * @param message the message containing the details of the customer to be created.
+     */
+    void createCustomer(CreateCustomerMessage message);
 
 
     /**
-     * Creates a new customer and saves it to the repository.
+     * Initiates the email verification process for a customer based on their authentication details.
      *
-     * @param message Contains data for creating a new customer including id, email, and username.
+     * @param authentication the authentication details of the current user, used to identify the customer.
      */
-    public void createCustomer(CreateCustomerMessage message) {
-        Customer newCustomer = Customer.builder()
-                .id(message.id())
-                .email(message.email())
-                .theme(DEFAULT_THEME)
-                .preferredLanguage(DEFAULT_LANGUAGE)
-                .profileIsCompleted(DEFAULT_PROFILE_IS_COMPLETED)
-                .isEmailVerified(DEFAULT_EMAIL_VERIFIED)
-                .receiveNewsletter(DEFAULT_RECEIVE_NEWSLETTER)
-                .enableNotifications(DEFAULT_ENABLE_NOTIFICATIONS)
-                .username(message.username())
-                .build();
-        repository.save(newCustomer);
-    }
+    void startEmailVerification(Authentication authentication);
 
-    public void startEmailVerification(Authentication authentication) {
-        Customer customer = getCustomerFromAuth(authentication);
+    /**
+     * Verifies the provided email verification code for the authenticated customer.
+     *
+     * @param verifyCode the email verification code sent to the customer's email.
+     * @param authentication the authentication details of the current user, used to identify the customer.
+     */
+    void verifyCode(String verifyCode, Authentication authentication);
 
-        if (customer.isEmailVerified()) {
-            log.error("Email {} is already verified", customer.getEmail());
-            throw new VerificationCodeException("Email is already verified");
-        }
+    /**
+     * Finds all customers based on the provided filter criteria.
+     *
+     * @param page the page number of the results to retrieve.
+     * @param pageSize the number of customers to retrieve per page.
+     * @param country the country to filter customers by.
+     * @param email the email pattern to filter customers by.
+     * @param id the ID to filter customers by.
+     * @param isEmailVerified flag to filter customers based on their email verification status.
+     * @param username the username pattern to filter customers by.
+     * @return a paginated response containing a list of customer DTOs.
+     */
+    PaginationResponse<List<CustomerDto>> findAllByFilter(Integer page, Integer pageSize, String country, String email, String id, boolean isEmailVerified, String username);
 
-        emailVerificationService.sendVerificationEmail(customer.getEmail());
-    }
+    /**
+     * Finds a customer by their unique identifier.
+     *
+     * @param customerId the unique identifier of the customer to find.
+     * @return the customer DTO containing the customer's details.
+     */
+    CustomerDto findById(UUID customerId);
 
-    public void verifyCode(String verifyCode, Authentication authentication) {
-        Customer customer = getCustomerFromAuth(authentication);
+    /**
+     * Updates the details of an existing customer.
+     *
+     * @param customerId the unique identifier of the customer to update.
+     * @param customerDto the DTO containing the updated details of the customer.
+     * @param authentication the authentication details of the current user, used to verify permissions.
+     */
+    void updateCustomerDetails(UUID customerId, CustomerDto customerDto, Authentication authentication);
 
-        try {
-            String verifiedEmail = emailVerificationService.verifyEmail(verifyCode);
-            if (!verifiedEmail.equals(customer.getEmail())) {
-                String errorMsg = String.format("Email %s does not match the authenticated user %s", verifiedEmail, customer.getEmail());
-                log.error(errorMsg);
-                throw new VerificationCodeException("Invalid verification code");
-            }
-            customer.setEmailVerified(true);
-            repository.save(customer);
-        } catch (VerificationCodeException e) {
-            log.error("Verification code {} is invalid or expired", verifyCode);
-            throw e;
-        }
-    }
-
-    private Customer getCustomerFromAuth(Authentication authentication) {
-        JwtUser jwtUser = (JwtUser) authentication.getPrincipal();
-        return repository.findById(jwtUser.getId()).orElseThrow(() -> new RuntimeException("Customer not found"));
-    }
-
-    public PaginationResponse<List<CustomerDto>> findAllByFilter(Integer page, Integer pageSize, String country, String email, String id, boolean isEmailVerified, String username) {
-        Pageable pageable = PageRequest.of(page, pageSize);
-
-        Specification<Customer> spec = Specification.where(
-                        Optional.ofNullable(country).map(CustomerSpecification::hasCountry).orElse(null))
-                .and(Optional.ofNullable(email).map(CustomerSpecification::emailStartsWith).orElse(null))
-                .and(Optional.ofNullable(username).map(CustomerSpecification::usernameStartsWith).orElse(null))
-                .and(CustomerSpecification.isEmailVerified(isEmailVerified));
-
-        Page<Customer> customerPage = repository.findAll(spec, pageable);
-
-        return new PaginationResponse<>(
-                customerPage.getTotalPages(),
-                customerPage.getNumber(),
-                customerPage.getSize(),
-                customerPage.getContent().stream().map(CustomerDto::to).toList()
-        );
-    }
-
-    public CustomerDto findById(UUID customerId) {
-        return repository.findById(customerId).map(CustomerDto::to).orElseThrow(() -> new RuntimeException("Customer not found"));
-    }
 }
