7de9fec979996ea1caa6f8a74028856e8f6a4022
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 00:00:23 +0200

    Refactor Director entity to extend Person class
    Replaced BaseEntity superclass with Person to avoid redundancy. Removed name field and validation annotation since it's now inherited from the Person class.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Director.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Director.java
@@ -8,11 +8,9 @@
 
 package io.github.marianciuc.streamingservice.content.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,11 +24,7 @@ import java.util.List;
 @AllArgsConstructor
 @SuperBuilder
 @Table(name = "directors")
-public class Director extends BaseEntity {
-
-    @NotNull
-    @Column(name = "name", nullable = false)
-    private String name;
+public class Director extends Person {
 
     @ManyToMany(mappedBy = "directors")
     private List<Content> contents;
