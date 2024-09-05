3c34fa433ead54817e7f648ecdeea683ccf07f3e
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 19:35:07 +0200

    Rename CustomerDataDto to CustomerDto and add transformation method.
    The class has been renamed for brevity and clarity. Added a static method to convert a Customer object to a CustomerDto, enhancing data transformation capabilities.

---
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/dto/CustomerDto.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: CustomerDataDto.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.customer.dto;
+
+import io.github.marianciuc.streamingservice.customer.model.Customer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record CustomerDto(
+        UUID id,
+        String theme,
+        String email,
+        LocalDate birthDate,
+        String country,
+        String username,
+        String profilePicture,
+        String preferredLanguage,
+        boolean isEmailVerified,
+        boolean receiveNewsletter,
+        boolean enableNotifications,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
+    public static CustomerDto to(Customer customer) {
+        return new CustomerDto(
+                customer.getId(),
+                customer.getTheme(),
+                customer.getEmail(),
+                customer.getBirthDate(),
+                customer.getCountry(),
+                customer.getUsername(),
+                customer.getProfilePicture(),
+                customer.getPreferredLanguage(),
+                customer.isEmailVerified(),
+                customer.isReceiveNewsletter(),
+                customer.isEnableNotifications(),
+                customer.getCreatedAt(),
+                customer.getUpdatedAt()
+        );
+    }
+}
