e915ba951f4fd3b930601f92ba68f2e194170d4c
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 19:34:55 +0200

    Add updatedAt field to Customer entity
    Introduce the updatedAt field with LastModifiedDate annotation to track the last modification time of Customer entities. This enhancement supports auditing and better management of entity changes over time.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/model/Customer.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/model/Customer.java
@@ -17,6 +17,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -68,4 +69,8 @@ public class Customer {
     @CreatedDate
     @Column(name = "created_at")
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
