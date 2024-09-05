dd6a3dc4e52ad6598e84e153b5db04912bafb477
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 23:59:41 +0200

    Add method to find content by ID and record status
    Introduced a new method `findByIdAndRecordStatus` in `ContentRepository`. This allows fetching content based on its ID and current record status, enhancing query capabilities.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/repository/ContentRepository.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/repository/ContentRepository.java
@@ -1,14 +1,20 @@
 package io.github.marianciuc.streamingservice.content.repository;
 
+import io.github.marianciuc.streamingservice.content.dto.ContentDto;
 import io.github.marianciuc.streamingservice.content.entity.Content;
+import io.github.marianciuc.streamingservice.content.enums.RecordStatus;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ContentRepository extends JpaRepository<Content, UUID>, JpaSpecificationExecutor<Content> {
+
     Page<Content> findAll(Specification<Content> spec, Pageable pageable);
+    Optional<Content> findByIdAndRecordStatus(UUID id, RecordStatus recordStatus);
 }
