34dd0e53484b4673fb0a3d446405850ec69529a2
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 20:36:59 +0200

    Refactor CustomerConsumer to use CustomerServiceImpl
    Replaced the generic CustomerService with its implementation, CustomerServiceImpl, within the CustomerConsumer class. This change ensures that the actual implementation methods are invoked directly, improving code clarity and specificity.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/kafka/CustomerConsumer.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/kafka/CustomerConsumer.java
@@ -9,7 +9,7 @@
 package io.github.marianciuc.streamingservice.customer.kafka;
 
 import io.github.marianciuc.streamingservice.customer.dto.CreateCustomerMessage;
-import io.github.marianciuc.streamingservice.customer.services.CustomerService;
+import io.github.marianciuc.streamingservice.customer.services.impl.CustomerServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -18,11 +18,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CustomerConsumer {
 
-    private final CustomerService customerService;
+    private final CustomerServiceImpl customerServiceImpl;
 
     @KafkaListener(topics = "customer-creation", groupId = "customers")
     public void consume(CreateCustomerMessage message) {
         System.out.println("Consumed message: " + message);
-        customerService.createCustomer(message);
+        customerServiceImpl.createCustomer(message);
     }
 }
