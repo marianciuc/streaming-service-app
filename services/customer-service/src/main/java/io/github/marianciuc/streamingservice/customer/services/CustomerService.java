08ee2508cd3c7297318bf7380796f18913b33775
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 19:36:59 +0200

    Add customer filtering and pagination methods to CustomerService
    Introduced methods to filter customers based on specified criteria and to handle pagination. Implemented necessary pagination response and customer DTO conversions for better response handling.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/services/CustomerService.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/services/CustomerService.java
@@ -10,14 +10,29 @@ package io.github.marianciuc.streamingservice.customer.services;
 
 import io.github.marianciuc.jwtsecurity.entity.JwtUser;
 import io.github.marianciuc.streamingservice.customer.dto.CreateCustomerMessage;
+import io.github.marianciuc.streamingservice.customer.dto.CustomerDto;
+import io.github.marianciuc.streamingservice.customer.dto.PaginationResponse;
 import io.github.marianciuc.streamingservice.customer.exceptions.VerificationCodeException;
 import io.github.marianciuc.streamingservice.customer.model.Customer;
 import io.github.marianciuc.streamingservice.customer.repository.CustomerRepository;
+import io.github.marianciuc.streamingservice.customer.specifications.CustomerSpecification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+/**
+ * Service class that handles operations related to customers, such as creating a customer,
+ * starting email verification, verifying email codes, and finding customers based on filters.
+ */
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -34,6 +49,11 @@ public class CustomerService {
     private final EmailVerificationService emailVerificationService;
 
 
+    /**
+     * Creates a new customer and saves it to the repository.
+     *
+     * @param message Contains data for creating a new customer including id, email, and username.
+     */
     public void createCustomer(CreateCustomerMessage message) {
         Customer newCustomer = Customer.builder()
                 .id(message.id())
@@ -81,5 +101,28 @@ public class CustomerService {
     private Customer getCustomerFromAuth(Authentication authentication) {
         JwtUser jwtUser = (JwtUser) authentication.getPrincipal();
         return repository.findById(jwtUser.getId()).orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+    public PaginationResponse<List<CustomerDto>> findAllByFilter(Integer page, Integer pageSize, String country, String email, String id, boolean isEmailVerified, String username) {
+        Pageable pageable = PageRequest.of(page, pageSize);
+
+        Specification<Customer> spec = Specification.where(
+                        Optional.ofNullable(country).map(CustomerSpecification::hasCountry).orElse(null))
+                .and(Optional.ofNullable(email).map(CustomerSpecification::emailStartsWith).orElse(null))
+                .and(Optional.ofNullable(username).map(CustomerSpecification::usernameStartsWith).orElse(null))
+                .and(CustomerSpecification.isEmailVerified(isEmailVerified));
+
+        Page<Customer> customerPage = repository.findAll(spec, pageable);
+
+        return new PaginationResponse<>(
+                customerPage.getTotalPages(),
+                customerPage.getNumber(),
+                customerPage.getSize(),
+                customerPage.getContent().stream().map(CustomerDto::to).toList()
+        );
+    }
+
+    public CustomerDto findById(UUID customerId) {
+        return repository.findById(customerId).map(CustomerDto::to).orElseThrow(() -> new RuntimeException("Customer not found"));
     }
 }
