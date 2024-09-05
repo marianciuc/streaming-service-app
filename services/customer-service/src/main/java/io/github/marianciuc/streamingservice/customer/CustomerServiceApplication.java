ff8b79e02fb493f7cff3b99e36dee6406b474f18
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 16:57:17 +0200

    Enable discovery client, Feign, and JPA auditing
    Add annotations to enable service discovery, Feign clients, and JPA auditing in CustomerServiceApplication. This facilitates better integration and management of microservices and data auditing.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/CustomerServiceApplication.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/CustomerServiceApplication.java
@@ -2,8 +2,14 @@ package io.github.marianciuc.streamingservice.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableJpaAuditing
 public class CustomerServiceApplication {
 
     public static void main(String[] args) {
