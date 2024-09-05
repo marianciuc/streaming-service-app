797e47cb14475c64be3fb6da4b3b0d71f51fb177
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-05 20:37:41 +0200

    Add Javadoc comments to EmailVerificationService
    Provided detailed Javadoc comments for the methods in the EmailVerificationService interface. These comments describe the purpose, parameters, and potential exceptions associated with each method, enhancing code readability and maintainability.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/services/EmailVerificationService.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/services/EmailVerificationService.java
@@ -10,7 +10,24 @@ package io.github.marianciuc.streamingservice.customer.services;
 
 import io.github.marianciuc.streamingservice.customer.exceptions.VerificationCodeException;
 
+/**
+ * Service interface for managing email verification processes.
+ */
 public interface EmailVerificationService {
+
+    /**
+     * Sends an email containing a verification code to the specified email address.
+     *
+     * @param email the email address to which the verification email will be sent.
+     */
     void sendVerificationEmail(String email);
+
+    /**
+     * Verifies the provided email verification code and returns the corresponding email address.
+     *
+     * @param verifyCode the email verification code to validate.
+     * @return the email address associated with the provided verification code.
+     * @throws VerificationCodeException if the verification code is invalid or expired.
+     */
     String verifyEmail(String verifyCode) throws VerificationCodeException;
 }
