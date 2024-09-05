7f1397a588b13e6ce741f8c52c7c7c2232d58470
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 00:00:34 +0200

    Add duration and masterPlaylistUrl fields to Movie entity
    This commit introduces two new fields, `duration` and `masterPlaylistUrl`, to the `Movie` entity. These additions will allow tracking of movie length and corresponding playlist URL directly within the `Movie` entity.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Movie.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/entity/Movie.java
@@ -1,13 +1,11 @@
 package io.github.marianciuc.streamingservice.content.entity;
 
-import io.github.marianciuc.streamingservice.content.enums.RecordStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -21,6 +19,12 @@ public class Movie extends BaseEntity{
     @OneToOne(mappedBy = "movie")
     private Content content;
 
+    @Column(name = "duration")
+    private int duration;
+
+    @Column(name = "master_playlist_url")
+    private String masterPlaylistUrl;
+
     @Column(name = "master_playlist_id")
-    UUID masterPlaylistId;
+    private UUID masterPlaylistId;
 }
