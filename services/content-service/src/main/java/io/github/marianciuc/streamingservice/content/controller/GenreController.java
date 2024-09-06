fce18ee15d103f1f580b3acbc80dfbad891f7e98
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:59:40 +0200

    Implement CRUD operations in GenreController
    Added endpoints for creating, updating, getting by ID, deleting genres, and fetching all genres in GenreController. Enabled the GenreService to be used for handling these requests.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/GenreController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/GenreController.java
@@ -1,8 +1,12 @@
 package io.github.marianciuc.streamingservice.content.controller;
 
+import io.github.marianciuc.streamingservice.content.dto.GenreDto;
+import io.github.marianciuc.streamingservice.content.service.GenreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -10,35 +14,32 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class GenreController {
 
-//    public final GenreService genreService;
-//
-//    @GetMapping
-//    public ResponseEntity<PaginationResponse<GenreResponse>> getAllGenres(
-//            @RequestParam(name = "name", required = false) String name,
-//            @RequestParam(name = "page", required = false, defaultValue = "0") Integer page,
-//            @RequestParam(name = "size", required = false, defaultValue = "10") Integer size
-//    ) {
-//        return ResponseEntity.ok(genreService.findAll(name, page, size));
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<GenreResponse> findGenreById(@PathVariable("id") UUID id) {
-//        return ResponseEntity.ok(genreService.findGenreResponseById(id));
-//    }
-//
-//    @PostMapping
-//    public ResponseEntity<UUID> createGenre(@RequestBody @Valid GenreRequest genreRequest) {
-//        return ResponseEntity.status(HttpStatus.CREATED).body(genreService.createGenre(genreRequest));
-//    }
-//
-//    @PutMapping("/{id}")
-//    public ResponseEntity<UUID> updateGenreById(@PathVariable("id") UUID id, @Valid @RequestBody GenreRequest genreRequest) {
-//        return ResponseEntity.status(HttpStatus.OK).body(genreService.updateGenre(id, genreRequest));
-//    }
-//
-//    @DeleteMapping("/{id}")
-//    public ResponseEntity<Void> deleteGenreById(@PathVariable("id") UUID id) {
-//        genreService.deleteById(id);
-//        return ResponseEntity.ok().build();
-//    }
+    private final GenreService genreService;
+
+    @PostMapping
+    public ResponseEntity<UUID> createGenre(@RequestBody GenreDto request) {
+        return ResponseEntity.ok(genreService.createGenre(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<GenreDto> getGenreById(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(genreService.getGenre(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateGenre(@PathVariable("id") UUID id, @RequestBody GenreDto request) {
+        genreService.updateGenre(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteGenre(@PathVariable("id") UUID id) {
+        genreService.deleteGenre(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<GenreDto>> getAllGenres() {
+        return ResponseEntity.ok(genreService.getAllGenres());
+    }
 }
