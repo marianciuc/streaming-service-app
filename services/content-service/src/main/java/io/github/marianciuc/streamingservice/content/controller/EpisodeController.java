de2481c848245fa4dadb0ea38e0b226bd80894ea
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:59:13 +0200

    Add CRUD operations to EpisodeController
    Implemented create, retrieve, update, and delete operations for episodes in EpisodeController. This includes endpoints to handle episode creation, fetching details by ID and parameters, updating, and deleting episodes using EpisodeService.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/EpisodeController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/EpisodeController.java
@@ -8,5 +8,47 @@
 
 package io.github.marianciuc.streamingservice.content.controller;
 
+import io.github.marianciuc.streamingservice.content.dto.EpisodeDto;
+import io.github.marianciuc.streamingservice.content.service.EpisodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController("/api/v1/episodes")
+@RequiredArgsConstructor
 public class EpisodeController {
+
+    private final EpisodeService episodeService;
+
+    @PostMapping
+    public ResponseEntity<UUID> createEpisode(@RequestBody EpisodeDto episodeDto) {
+        return ResponseEntity.ok(episodeService.createEpisode(episodeDto));
+    }
+
+    @GetMapping("/content/{content-id}/season/{season-number}/episode/{episode-number}")
+    public ResponseEntity<EpisodeDto> getEpisodeByParams(
+            @PathVariable("content-id") UUID contentId,
+            @PathVariable("season-number") Integer seasonNumber,
+            @PathVariable("episode-number") Integer episodeNumber) {
+        return ResponseEntity.ok(episodeService.findEpisodeByParams(contentId, seasonNumber, episodeNumber));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EpisodeDto> getEpisodeById(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(episodeService.getEpisode(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateEpisode(@PathVariable("id") UUID id, @RequestBody EpisodeDto episodeDto) {
+        episodeService.updateEpisode(id, episodeDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteEpisode(@PathVariable("id") UUID id) {
+        episodeService.deleteEpisode(id);
+        return ResponseEntity.ok().build();
+    }
 }
