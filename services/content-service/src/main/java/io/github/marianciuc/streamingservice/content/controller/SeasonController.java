fc131673e26bff5fe89f56bc8deb3eebd494f84a
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-07 00:18:17 +0200

    Add CRUD endpoints to SeasonController
    Implemented create, retrieve, update, and delete operations for seasons. Each method interacts with the SeasonService to perform the respective actions, ensuring the application's season management is fully operational.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/SeasonController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/SeasonController.java
@@ -8,5 +8,47 @@
 
 package io.github.marianciuc.streamingservice.content.controller;
 
+import io.github.marianciuc.streamingservice.content.dto.SeasonDto;
+import io.github.marianciuc.streamingservice.content.service.SeasonService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController("/api/v1/seasons")
+@RequiredArgsConstructor
 public class SeasonController {
+
+    private final SeasonService seasonService;
+
+    @PostMapping
+    public ResponseEntity<UUID> createSeason(@RequestBody @Valid SeasonDto request) {
+        return ResponseEntity.ok(seasonService.createSeason(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<SeasonDto> getSeasonById(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(seasonService.findSeason(id));
+    }
+
+    @GetMapping("/content/{content-id}/season/{season-number}")
+    public ResponseEntity<SeasonDto> getSeasonByParams(
+            @PathVariable("content-id") UUID contentId,
+            @PathVariable("season-number") Integer seasonNumber) {
+        return ResponseEntity.ok(seasonService.findSeasonByParams(contentId, seasonNumber));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateSeason(@PathVariable("id") UUID id, @RequestBody @Valid SeasonDto request) {
+        seasonService.updateSeason(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSeason(@PathVariable("id") UUID id) {
+        seasonService.deleteSeason(id);
+        return ResponseEntity.ok().build();
+    }
 }
