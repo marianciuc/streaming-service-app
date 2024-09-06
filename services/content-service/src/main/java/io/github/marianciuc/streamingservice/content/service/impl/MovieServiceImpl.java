1c93cfa3d7d513a272c6cfa95ce40e6ce378e4af
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 22:00:22 +0200

    Enhance MovieService with repository operations
    Added CRUD operations to MovieServiceImpl including creating, retrieving, updating, and deleting movies. Integrated new dependencies like ContentService and MovieRepository, and added exception handling for not found movies.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/impl/MovieServiceImpl.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/impl/MovieServiceImpl.java
@@ -10,7 +10,13 @@ package io.github.marianciuc.streamingservice.content.service.impl;
 
 import io.github.marianciuc.streamingservice.content.dto.CreateMasterPlayListMessage;
 import io.github.marianciuc.streamingservice.content.dto.MovieDto;
+import io.github.marianciuc.streamingservice.content.entity.Content;
+import io.github.marianciuc.streamingservice.content.entity.Movie;
+import io.github.marianciuc.streamingservice.content.enums.RecordStatus;
+import io.github.marianciuc.streamingservice.content.exceptions.NotFoundException;
 import io.github.marianciuc.streamingservice.content.kafka.KafkaMediaProducer;
+import io.github.marianciuc.streamingservice.content.repository.MovieRepository;
+import io.github.marianciuc.streamingservice.content.service.ContentService;
 import io.github.marianciuc.streamingservice.content.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,30 +28,46 @@ import java.util.UUID;
 public class MovieServiceImpl implements MovieService {
 
     private final KafkaMediaProducer kafkaMediaProducer;
+    private final ContentService contentService;
+    private final MovieRepository repository;
 
     @Override
     public UUID createMovie(MovieDto movieDto) {
+        Content content = contentService.getContentEntity(movieDto.contentId());
 
-        return null;
+        Movie movie = Movie.builder()
+                .duration(movieDto.duration())
+                .recordStatus(RecordStatus.HIDDEN)
+                .content(content)
+                .build();
+        return repository.save(movie).getId();
     }
 
     @Override
     public MovieDto getMovie(UUID movieId) {
-        return null;
+        return MovieDto.toMovieDto(this.getMovieEntity(movieId));
     }
 
     @Override
     public void updateMovieMasterPlaylist(CreateMasterPlayListMessage message) {
+        Movie movie = this.getMovieEntity(message.contentId());
+        movie.setMasterPlaylistId(message.masterPlaylistId());
 
+        if (!message.url().isEmpty()) {
+            movie.setMasterPlaylistUrl(message.url());
+            movie.setRecordStatus(RecordStatus.ACTIVE);
+        }
+        repository.save(movie);
     }
 
     @Override
     public void deleteMovie(UUID movieId) {
         kafkaMediaProducer.sendDeleteMediaMessage(movieId);
+        repository.deleteById(movieId);
     }
 
-    @Override
-    public void getMovieById(UUID movieId) {
-
+    private Movie getMovieEntity(UUID contentId) {
+        return repository.findByContentId(contentId)
+                .orElseThrow(() -> new NotFoundException("Movie not found"));
     }
 }
