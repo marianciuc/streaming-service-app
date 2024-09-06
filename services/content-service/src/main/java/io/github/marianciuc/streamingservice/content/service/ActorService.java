6896c2900d4a095e04ff828fd22f47bab94797e0
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:58:37 +0200

    Enhance ActorService interface with detailed Javadoc
    Added detailed Javadoc comments for methods in the ActorService interface. This improvement provides clear documentation on the purpose, parameters, and expected outcomes for each method, aiding developers in understanding and utilizing the service more effectively.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/ActorService.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/ActorService.java
@@ -10,18 +10,54 @@ package io.github.marianciuc.streamingservice.content.service;
 
 import io.github.marianciuc.streamingservice.content.dto.PersonDto;
 import io.github.marianciuc.streamingservice.content.entity.Actor;
+import io.github.marianciuc.streamingservice.content.exceptions.NotFoundException;
 
 import java.util.UUID;
 
+/**
+ * The service interface for the actor entity
+ */
 public interface ActorService {
 
+    /**
+     * Creates an actor with the given data transfer object
+     *
+     * @param personDto - the person data transfer object
+     * @return the UUID of the created actor
+     */
     UUID createActor(PersonDto personDto);
 
+    /**
+     * Deletes an actor by the given UUID from the database
+     *
+     * @param id - the UUID of the actor to be deleted
+     */
     void deleteActor(UUID id);
 
+
+    /**
+     * Updates an actor with the given data transfer object where fields are not null
+     *
+     * @param id        - the UUID of the actor to be updated
+     * @param personDto - the person data transfer object
+     */
     void updateActor(UUID id, PersonDto personDto);
 
+
+    /**
+     * Finds an actor by the given UUID and returns the person data transfer object
+     *
+     * @param id - the UUID of the actor to be retrieved
+     * @return the person data transfer object of the actor with the given UUID
+     * @throws NotFoundException if the actor with the given UUID is not found
+     */
     PersonDto findActorById(UUID id);
 
+    /**
+     * Finds an actor by the given UUID and returns the actor entity
+     * @param id the UUID of the actor to be retrieved
+     * @return the actor entity with the given UUID
+     * @throws NotFoundException if the actor with the given UUID is not found
+     */
     Actor findActorEntityById(UUID id);
 }
