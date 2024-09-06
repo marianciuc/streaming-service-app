1e1b30ddbeb546c3dd5621d41fb7f6241001299c
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 22:00:07 +0200

    Implement CRUD operations in TagsController
    Added methods for fetching all tags, retrieving a tag by ID, and deleting a tag. Modified updateTag method for consistency in path variable naming. Additionally, introduced DTO for tag responses.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/TagsController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/TagsController.java
@@ -8,11 +8,13 @@
 
 package io.github.marianciuc.streamingservice.content.controller;
 
+import io.github.marianciuc.streamingservice.content.dto.TagDto;
 import io.github.marianciuc.streamingservice.content.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController("/api/v1/content/tags")
@@ -26,9 +28,25 @@ public class TagsController {
         return ResponseEntity.ok(tagService.createTag(tagName));
     }
 
-    @PutMapping("/{tag-id}")
-    public ResponseEntity<Void> updateTag(@PathVariable("tag-id") UUID id, @RequestParam("tag") String tagName) {
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateTag(@PathVariable("id") UUID id, @RequestParam("tag") String tagName) {
         tagService.updateTag(id, tagName);
         return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTag(@PathVariable("id") UUID id) {
+        tagService.deleteTag(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TagDto>> getAllTags(@PathVariable("query") String query) {
+        return ResponseEntity.ok(tagService.findTagByNamePrefix(query));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TagDto> getTagById(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(TagDto.toTagDto(tagService.findTagById(id)));
     }
 }
