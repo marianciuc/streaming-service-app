a572bb1d0820a9e98c1f369b353626c818205d92
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:58:32 +0200

    Add CRUD operations to ActorController
    Implemented the @RestController with endpoints for adding, updating, deleting, and fetching actors. These operations leverage ActorService to interact with actor data, ensuring valid input through @Valid annotations.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/ActorController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/ActorController.java
@@ -8,5 +8,40 @@
 
 package io.github.marianciuc.streamingservice.content.controller;
 
+import io.github.marianciuc.streamingservice.content.dto.PersonDto;
+import io.github.marianciuc.streamingservice.content.service.ActorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController("/api/v1/actors")
+@RequiredArgsConstructor
 public class ActorController {
+
+    private final ActorService actorService;
+
+    @PostMapping
+    public ResponseEntity<UUID> addActor(@Valid @RequestBody PersonDto request) {
+        return ResponseEntity.ok(actorService.createActor(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateActor(@PathVariable UUID  id, @RequestBody PersonDto request) {
+        actorService.updateActor(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteActor(@PathVariable UUID id) {
+        actorService.deleteActor(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonDto> getActor(@PathVariable UUID id) {
+        return ResponseEntity.ok(actorService.findActorById(id));
+    }
 }
