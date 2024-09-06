86e227c27551fed339a169311f444f341597f32a
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:59:03 +0200

    Add Javadoc comments and NotFoundException to DirectorService
    Introduced detailed Javadoc comments for all methods in the DirectorService interface for better documentation. Also added a NotFoundException for handling cases where a director is not found by its ID.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/DirectorService.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/DirectorService.java
@@ -10,13 +10,50 @@ package io.github.marianciuc.streamingservice.content.service;
 
 import io.github.marianciuc.streamingservice.content.dto.PersonDto;
 import io.github.marianciuc.streamingservice.content.entity.Director;
+import io.github.marianciuc.streamingservice.content.exceptions.NotFoundException;
 
 import java.util.UUID;
 
+/**
+ * Service interface for managing directors.
+ */
 public interface DirectorService {
+
+    /**
+     * Creates a new director by the given director DTO.
+     * @param personDto the director DTO
+     * @return the ID of the created director
+     */
     UUID createDirector(PersonDto personDto);
+
+    /**
+     * Deletes the director with the given ID.
+     * @param id the ID of the director to delete
+     * @throws NotFoundException if the director with the given ID is not found
+     */
     void deleteDirector(UUID id);
+
+    /**
+     * Updates the director with the given ID by the given director DTO. Updates only the fields that are not null.
+     * @param id the ID of the director to update
+     * @param personDto the director DTO
+     * @throws NotFoundException if the director with the given ID is not found
+     */
     void updateDirector(UUID id, PersonDto personDto);
+
+    /**
+     * Retrieves a director by its ID.
+     * @param id the ID of the director to retrieve
+     * @return the PersonDto object representing the director
+     * @throws NotFoundException if the director with the given ID is not found
+     */
     PersonDto findDirectorById(UUID id);
+
+    /**
+     * Retrieves a director entity by its ID.
+     * @param id the ID of the director entity to retrieve
+     * @return the Director object representing the director entity
+     * @throws NotFoundException if the director entity with the given ID is not found
+     */
     Director findDirectorEntityById(UUID id);
 }
