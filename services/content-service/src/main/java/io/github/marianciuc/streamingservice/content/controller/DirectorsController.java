497a81e030ebb2a7f2f30243f7b32f3ad1fb0fcf
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:58:56 +0200

    Add CRUD endpoints to DirectorsController
    Implemented create, read, update, and delete (CRUD) operations for managing directors. This includes endpoints for creating, retrieving, updating, and deleting directors by ID. Each method returns an appropriate HTTP response.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/DirectorsController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/controller/DirectorsController.java
@@ -8,5 +8,41 @@
 
 package io.github.marianciuc.streamingservice.content.controller;
 
+
+import io.github.marianciuc.streamingservice.content.dto.PersonDto;
+import io.github.marianciuc.streamingservice.content.service.DirectorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController("/api/v1/directors")
+@RequiredArgsConstructor
 public class DirectorsController {
+
+    private final DirectorService directorService;
+
+    @PostMapping
+    public ResponseEntity<UUID> createDirector(@Valid @RequestBody PersonDto request) {
+        return ResponseEntity.ok(directorService.createDirector(request));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PersonDto> getDirectorById(@PathVariable UUID id) {
+        return ResponseEntity.ok(directorService.findDirectorById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateDirector(@PathVariable UUID id, @RequestBody PersonDto request) {
+        directorService.updateDirector(id, request);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDirector(@PathVariable UUID id) {
+        directorService.deleteDirector(id);
+        return ResponseEntity.ok().build();
+    }
 }
