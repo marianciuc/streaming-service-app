6fdae2ab1761cef22f15041283520b1641585231
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:59:49 +0200

    Refactor GenreServiceImpl to implement CRUD operations
    Modified the GenreServiceImpl class to fully implement the CRUD operations, enabling create, read, update, and delete functionalities for Genre entities. Introduced necessary exceptions, updated method logic, and refined repository usage.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/impl/GenreServiceImpl.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/service/impl/GenreServiceImpl.java
@@ -10,6 +10,8 @@ package io.github.marianciuc.streamingservice.content.service.impl;
 
 import io.github.marianciuc.streamingservice.content.dto.GenreDto;
 import io.github.marianciuc.streamingservice.content.entity.Genre;
+import io.github.marianciuc.streamingservice.content.enums.RecordStatus;
+import io.github.marianciuc.streamingservice.content.exceptions.NotFoundException;
 import io.github.marianciuc.streamingservice.content.repository.GenreRepository;
 import io.github.marianciuc.streamingservice.content.service.GenreService;
 import lombok.RequiredArgsConstructor;
@@ -22,35 +24,44 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class GenreServiceImpl implements GenreService {
 
-    private final GenreRepository genreRepository;
+    private final GenreRepository repository;
 
     @Override
     public UUID createGenre(GenreDto genreDto) {
-        return null;
+        Genre genre = Genre.builder()
+                .title(genreDto.title())
+                .description(genreDto.description())
+                .recordStatus(RecordStatus.ACTIVE)
+                .build();
+        return repository.save(genre).getId();
     }
 
     @Override
     public GenreDto getGenre(UUID id) {
-        return null;
+        return GenreDto.toGenreDto(this.getGenreEntity(id));
     }
 
     @Override
     public void updateGenre(UUID id, GenreDto genreDto) {
-
+        Genre genre = this.getGenreEntity(id);
+        if (!genreDto.title().isEmpty()) genre.setTitle(genreDto.title());
+        if (!genreDto.description().isEmpty()) genre.setDescription(genreDto.description());
+        repository.save(genre);
     }
 
     @Override
     public void deleteGenre(UUID id) {
-
+        Genre genre = this.getGenreEntity(id);
+        repository.delete(genre);
     }
 
     @Override
     public Genre getGenreEntity(UUID id) {
-        return null;
+        return repository.findById(id).orElseThrow(()-> new NotFoundException("Genre not found"));
     }
 
     @Override
     public List<GenreDto> getAllGenres() {
-        return List.of();
+        return repository.findAll().stream().map(GenreDto::toGenreDto).toList();
     }
 }
