a14b543461b69bf5be4e2be168f2454a8fb31dff
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-06 21:59:35 +0200

    Remove exception handling logic from ExceptionHandlerController
    This change removes the logic that handles NotFoundException within the ExceptionHandlerController. The related private method constructResponseEntity has also been eliminated. This prepares the code for a new exception handling strategy.

--- services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/handler/ExceptionHandlerController.java
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/handler/ExceptionHandlerController.java
@@ -1,39 +1,9 @@
 package io.github.marianciuc.streamingservice.content.handler;
 
 
-import io.github.marianciuc.streamingservice.content.dto.response.ExceptionResponse;
-import jakarta.ws.rs.NotFoundException;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
 
-import java.time.LocalDateTime;
-
-/**
- * The ExceptionHandlerController class is responsible for handling exceptions thrown by RESTful APIs
- * and constructing appropriate ResponseEntity objects.
- */
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
-    /**
-     * Constructs a ResponseEntity containing an ExceptionResponse object with the given HttpStatus and message.
-     *
-     * @param status  the HttpStatus of the ResponseEntity
-     * @param message the message to be included in the ExceptionResponse object
-     * @return a ResponseEntity containing an ExceptionResponse object with the given HttpStatus and message
-     */
-    private ResponseEntity<ExceptionResponse> constructResponseEntity(HttpStatus status, String message) {
-        return ResponseEntity.status(status).body(new ExceptionResponse(message, LocalDateTime.now()));
-    }
-
-
-    @ExceptionHandler(NotFoundException.class)
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    protected ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException ex, WebRequest request) {
-        return constructResponseEntity(HttpStatus.NOT_FOUND, ex.getMessage());
-    }
 }
