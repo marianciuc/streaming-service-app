162348a039723565ab3f21f4d611dfa22174206d
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 22:05:16 +0200

    Refactor import paths for DTO classes.
    Updated import paths for RegistrationRequest and UserResponse to reflect new package structure. This increases modularity and improves overall code organization within the project.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/mappers/UserMapper.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/mappers/UserMapper.java
@@ -1,7 +1,7 @@
 package io.github.marianciuc.streamingservice.user.mappers;
 
-import io.github.marianciuc.streamingservice.user.dto.RegistrationRequest;
-import io.github.marianciuc.streamingservice.user.dto.UserResponse;
+import io.github.marianciuc.streamingservice.user.dto.requests.RegistrationRequest;
+import io.github.marianciuc.streamingservice.user.dto.responses.UserResponse;
 import io.github.marianciuc.streamingservice.user.entity.User;
 import io.github.marianciuc.streamingservice.user.enums.Role;
 import org.springframework.stereotype.Component;
