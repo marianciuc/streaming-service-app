7b576e816992e4d653c186314d267c691e21d981
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-09 00:01:15 +0200

    Update AccessTokenFactory to use tokenId instead of userId
    Changed the AccessTokenFactory to reference tokenId where userId was previously used. Added UUID import to support tokenId generation. This ensures better identification and uniqueness of tokens.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/factories/AccessTokenFactory.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/factories/AccessTokenFactory.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 
 @Setter
 public class AccessTokenFactory implements TokenFactory {
@@ -36,7 +37,7 @@ public class AccessTokenFactory implements TokenFactory {
         Instant now = Instant.now();
 
         return new Token(
-                token.userId(),
+                token.tokenId(),
                 token.userId(),
                 token.subject(),
                 token.issuer(),
