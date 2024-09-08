63b5e24c019b1030e7f65ae5bae303dfa9edfd4a
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 22:07:25 +0200

    Replace AuthService with UserService in AuthController
    Updated the AuthController to use UserService instead of AuthService for user registration. This includes changing the method signature to accept Authentication and adjusting import statements accordingly.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/AuthController.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/AuthController.java
@@ -1,11 +1,8 @@
 package io.github.marianciuc.streamingservice.user.controllers;
 
-import io.github.marianciuc.streamingservice.user.dto.CredentialsRequest;
-import io.github.marianciuc.streamingservice.user.dto.JsonWebTokenResponse;
-import io.github.marianciuc.streamingservice.user.dto.RegistrationRequest;
+import io.github.marianciuc.streamingservice.user.dto.requests.RegistrationRequest;
 import io.github.marianciuc.streamingservice.user.enums.APIPath;
-import io.github.marianciuc.streamingservice.user.services.AuthService;
-import io.github.marianciuc.streamingservice.user.services.impl.UserServiceImpl;
+import io.github.marianciuc.streamingservice.user.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -13,24 +10,17 @@ import org.springframework.web.bind.annotation.*;
 
 /**
  * The {@code AuthController} class handles API endpoints related to user authentication and authorization.
- * @see UserServiceImpl
  */
 @RestController
 @RequestMapping(APIPath.AUTH_V1_CONTROLLER)
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final AuthService authService;
+    private final UserService userService;
 
-    /**
-     * Registers a new user with the provided registration request details.
-     *
-     * @param request the registration request object containing the user details
-     * @return the user response object with the details of the registered user
-     */
     @PostMapping(APIPath.REGISTER)
-    public ResponseEntity<Void> register(@RequestBody RegistrationRequest request) {
-        authService.registerUser(request);
+    public ResponseEntity<Void> register(@RequestBody RegistrationRequest request, Authentication authentication) {
+        userService.createUser(request);
         return ResponseEntity.ok().build();
     }
 }
