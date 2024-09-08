9ff85f50652c3cde3ac070c9ae1bf3a23e408c8d
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:48:21 +0200

    Add AccessJWETokenStringDeserializer for token deserialization
    Introduce AccessJWETokenStringDeserializer class to handle the deserialization of JWE tokens into Token objects. This includes validating the token signature and extracting claims such as user details and authorities.

---
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/serializers/AccessJWETokenStringDeserializer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: AccessTokenStringDeserializer.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.user.serializers;
+
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.SignedJWT;
+import io.github.marianciuc.streamingservice.user.dto.Token;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.text.ParseException;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Slf4j
+public class AccessJWETokenStringDeserializer implements TokenDeserializer {
+
+    private final MACVerifier verifier;
+
+    /**
+     * Deserializes a token from a string representation
+     *
+     * @param token the token to be deserialized
+     * @return Token object or null if the token is invalid
+     */
+    @Override
+    public Token apply(String token) {
+        try {
+            var encryptedJWT = SignedJWT.parse(token);
+            if (encryptedJWT.verify(this.verifier)) {
+                return new Token(
+                        UUID.fromString(encryptedJWT.getHeader().getKeyID()),
+                        encryptedJWT.getJWTClaimsSet().getClaims().get("username").toString(),
+                        UUID.fromString(encryptedJWT.getJWTClaimsSet().getClaim("user-id").toString()),
+                        encryptedJWT.getJWTClaimsSet().getSubject(),
+                        encryptedJWT.getJWTClaimsSet().getIssuer(),
+                        encryptedJWT.getJWTClaimsSet().getStringListClaim("authorities"),
+                        encryptedJWT.getJWTClaimsSet().getIssueTime().toInstant(),
+                        encryptedJWT.getJWTClaimsSet().getExpirationTime().toInstant()
+                );
+            }
+            return null;
+        } catch (ParseException | JOSEException | IllegalArgumentException e) {
+            log.error("Error parsing JWT", e);
+            return null;
+        }
+    }
+}
