298b9d711f7189ade36c49dd8cd0186732e6f14e
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 22:05:38 +0200

    Add existsByEmailOrUsername method in UserRepository
    Introduced a method to check for the existence of users by email or username. This method employs validation constraints to ensure the email is valid and the username is not blank.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/repositories/UserRepository.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package io.github.marianciuc.streamingservice.user.repositories;
 
 import io.github.marianciuc.streamingservice.user.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -30,4 +32,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     @Query("SELECT u FROM User u WHERE u.username = :value OR u.email = :value")
     @Transactional(readOnly = true)
     Optional<User> findByEmailOrUsername(@Param("value") String value);
+
+    boolean existsByEmailOrUsername(@Email(message = "Email should be valid") String email, @NotBlank(message = "Username is mandatory") String username);
 }
