63773a5f8b3eda892176026871396faf63913e1f
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-09 01:46:53 +0200

    Refactor Kafka consumer for user creation events
    Renamed classes and variables to be more consistent with the new naming conventions for user events. Updated Kafka topic and group ID configurations to align with recent changes in the application setup.

--- services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/kafka/CustomerConsumer.java
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/kafka/CustomerConsumer.java
@@ -8,8 +8,8 @@
 
 package io.github.marianciuc.streamingservice.customer.kafka;
 
-import io.github.marianciuc.streamingservice.customer.dto.CreateCustomerMessage;
-import io.github.marianciuc.streamingservice.customer.services.impl.CustomerServiceImpl;
+import io.github.marianciuc.streamingservice.customer.kafka.messages.CreateUserMessage;
+import io.github.marianciuc.streamingservice.customer.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -18,11 +18,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class CustomerConsumer {
 
-    private final CustomerServiceImpl customerServiceImpl;
+    private final CustomerService customerService;
 
-    @KafkaListener(topics = "customer-creation", groupId = "customers")
-    public void consume(CreateCustomerMessage message) {
+    @KafkaListener(topics = "user-created-topic", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(CreateUserMessage message) {
         System.out.println("Consumed message: " + message);
-        customerServiceImpl.createCustomer(message);
+        customerService.createCustomer(message);
     }
 }
