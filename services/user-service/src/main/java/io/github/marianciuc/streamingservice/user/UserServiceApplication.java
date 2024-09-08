03dc0cdbac5367de1119506c25e8d04df5ca42ba
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:45:26 +0200

    Refactor indentation in main method.
    Corrected the indentation in the main method of UserServiceApplication.java to improve code readability. This change ensures consistent formatting according to the project's style guidelines.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/UserServiceApplication.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/UserServiceApplication.java
@@ -10,8 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 }
