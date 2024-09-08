2636c6f86bed1919cefefaa993f007955951f858
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-09 00:01:25 +0200

    Refactor AuthController endpoints and update access control
    Refactor API path constants to literal strings in AuthController. Removed redundant authentication parameter and added a permitAll() pre-authorization for the register endpoint.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/AuthController.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/AuthController.java
@@ -1,25 +1,25 @@
 package io.github.marianciuc.streamingservice.user.controllers;
 
 import io.github.marianciuc.streamingservice.user.dto.requests.RegistrationRequest;
-import io.github.marianciuc.streamingservice.user.enums.APIPath;
 import io.github.marianciuc.streamingservice.user.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * The {@code AuthController} class handles API endpoints related to user authentication and authorization.
  */
 @RestController
-@RequestMapping(APIPath.AUTH_V1_CONTROLLER)
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
 
-    @PostMapping(APIPath.REGISTER)
-    public ResponseEntity<Void> register(@RequestBody RegistrationRequest request, Authentication authentication) {
+    @PostMapping("/register")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity<Void> register(@RequestBody RegistrationRequest request) {
         userService.createUser(request);
         return ResponseEntity.ok().build();
     }
