91bf718b0babf85e8e05dd2f38fd7e6903d833b3
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 22:04:57 +0200

    Add IllegalArgumentException class to user exceptions.
    Introduce a custom IllegalArgumentException that extends RuntimeException. This new exception will be used to handle illegal argument errors more effectively within the user-service module.

---
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/exceptions/IllegalArgumentException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: IllegalArgumentException.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.user.exceptions;
+
+public class IllegalArgumentException extends RuntimeException{
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
