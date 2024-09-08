15bf2ed3a0c4c50e8ff1a84f400e1cb235901277
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 22:08:14 +0200

    Add Javadoc comments to JWTAuthenticationFilter
    Enhance the code clarity by adding Javadoc comments to the `JWTAuthenticationFilter` class, its constructor, and methods. This helps in understanding the purpose and usage of the class and its components.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/filters/JWTAuthenticationFilter.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/filters/JWTAuthenticationFilter.java
@@ -22,21 +22,47 @@ import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher
 
 import java.io.IOException;
 
+/**
+ * Filter that processes JWT authentication from the HTTP Authorization header.
+ */
 public class JWTAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
     private final AuthenticationConverter authenticationConverter;
 
+    /**
+     * Constructs a JWTAuthenticationFilter with the provided {@link AuthenticationConverter} and {@link AuthenticationManager}.
+     *
+     * @param authenticationConverter the converter to extract authentication from the request.
+     * @param authenticationManager the manager to handle the authentication process.
+     */
     public JWTAuthenticationFilter(AuthenticationConverter authenticationConverter, AuthenticationManager authenticationManager) {
         super(new RequestHeaderRequestMatcher(HttpHeaders.AUTHORIZATION));
         this.authenticationConverter = authenticationConverter;
         setAuthenticationManager(authenticationManager);
     }
 
+    /**
+     * Attempts to authenticate the request by extracting JWT token.
+     *
+     * @param request  the HTTP request object.
+     * @param response the HTTP response object.
+     * @return the Authentication object if successful, null otherwise.
+     * @throws AuthenticationException, IOException, ServletException if authentication attempt fails.
+     */
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
         return authenticationConverter.convert(request);
     }
 
+    /**
+     * Handles successful authentication by continuing the filter chain.
+     *
+     * @param request  the HTTP request object.
+     * @param response the HTTP response object.
+     * @param chain    the filter chain.
+     * @param authResult the result of the authentication process.
+     * @throws IOException, ServletException if an I/O error occurs.
+     */
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
         chain.doFilter(request, response);
