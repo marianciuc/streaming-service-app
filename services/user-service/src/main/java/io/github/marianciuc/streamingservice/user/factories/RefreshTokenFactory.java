cff9f72a8558fbbc0e4822e4ca7d09a4d51ebd16
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 22:06:56 +0200

    Update RefreshTokenFactory with JWTUserPrincipal support
    Replaced Token and User handling with JWTUserPrincipal to better align with authentication mechanisms. Enhanced authority mapping and added new methods for setting issuer and token time-to-live dynamically.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/factories/RefreshTokenFactory.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/factories/RefreshTokenFactory.java
@@ -9,8 +9,8 @@
 package io.github.marianciuc.streamingservice.user.factories;
 
 
-import io.github.marianciuc.streamingservice.user.dto.Token;
-import io.github.marianciuc.streamingservice.user.entity.User;
+import io.github.marianciuc.streamingservice.user.dto.common.Token;
+import io.github.marianciuc.streamingservice.user.entity.JWTUserPrincipal;
 import lombok.Setter;
 import org.springframework.security.core.Authentication;
 
@@ -23,35 +23,47 @@ import java.util.UUID;
 public class RefreshTokenFactory implements AuthenticationTokenFactory {
 
     private Duration tokenTtl = Duration.ofDays(1);
+
+
     private String issuer = "streaming-service";
 
     /**
      * Generates a new refresh token based on the authentication object. The new token will have the same user id, subject, issuer.
      * The authorities will be mapped to add {@code "GRAND::"} prefix and add {@code "REFRESH::REFRESH_TOKEN"} role.
+     *
      * @param authentication the authentication object to be used to generate the refresh token
      * @return a new refresh token
      */
     @Override
     public Token apply(Authentication authentication) {
         UUID userId = UUID.randomUUID();
-        if (authentication.getPrincipal() instanceof Token token) {
-            userId = token.userId();
-        } else if (authentication.getPrincipal() instanceof User user) {
-            userId = user.getId();
-        }
-        LinkedList<String> authorities = authentication.getAuthorities().stream()
-                .map(Object::toString).map("GRAND::"::concat).collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
-        authorities.add("REFRESH::REFRESH_TOKEN");
-        Instant now = Instant.now();
+        if (authentication.getPrincipal() instanceof JWTUserPrincipal jwtUserPrincipal) {
 
-        return new Token(
-                userId,
-                UUID.randomUUID(),
-                authentication.getName(),
-                issuer,
-                authorities,
-                now,
-                now.plus(this.tokenTtl)
-        );
+            LinkedList<String> authorities = authentication.getAuthorities().stream()
+                    .map(Object::toString).map("GRAND::"::concat).collect(LinkedList::new, LinkedList::add, LinkedList::addAll);
+            authorities.add("REFRESH::REFRESH_TOKEN");
+            Instant now = Instant.now();
+
+            return new Token(
+                    userId,
+                    jwtUserPrincipal.getId(),
+                    jwtUserPrincipal.getUsername(),
+                    issuer,
+                    authorities,
+                    now,
+                    now.plus(this.tokenTtl)
+            );
+        }
+        return null;
+    }
+
+    public RefreshTokenFactory issuer(String issuer) {
+        this.issuer = issuer;
+        return null;
+    }
+
+    public RefreshTokenFactory tokenTtl(Duration tokenTtl) {
+        this.tokenTtl = tokenTtl;
+        return null;
     }
 }
