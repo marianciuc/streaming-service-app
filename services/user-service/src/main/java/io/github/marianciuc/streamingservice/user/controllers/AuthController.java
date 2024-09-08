b75c5e075f10a9ebf731493f4896702afd6562f4
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:47:58 +0200

    Simplify register method in AuthController
    Replaced the return type of the register method to return a void response after invoking the authService. Removed obsolete login and refreshToken methods to streamline controller functionality.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/AuthController.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/AuthController.java
@@ -29,29 +29,8 @@ public class AuthController {
      * @return the user response object with the details of the registered user
      */
     @PostMapping(APIPath.REGISTER)
-    public ResponseEntity<JsonWebTokenResponse> register(@RequestBody RegistrationRequest request, Authentication authentication) {
-        return ResponseEntity.ok(authService.registerUser(request, authentication));
-    }
-
-    /**
-     * Authenticates a user with the provided credentials and returns a JSON Web Token response.
-     *
-     * @param request the CredentialsRequest object containing the user's login and password
-     * @return a ResponseEntity containing an object of type JsonWebTokenResponse with the access and refresh tokens
-     */
-    @PostMapping(APIPath.LOGIN)
-    public ResponseEntity<JsonWebTokenResponse> login(@RequestBody CredentialsRequest request) {
-        return ResponseEntity.ok(authService.authenticateUserByCredentials(request));
-    }
-
-    /**
-     * Refreshes the access and refresh tokens for the specified user.
-     *
-     * @param refreshToken the refresh token used to generate new tokens
-     * @return an object of type JsonWebTokenResponse containing the new access and refresh tokens
-     */
-    @PostMapping(APIPath.REFRESH)
-    public ResponseEntity<JsonWebTokenResponse> refreshToken(@RequestParam("token") String refreshToken) {
-        return ResponseEntity.ok(authService.refreshToken(refreshToken));
+    public ResponseEntity<Void> register(@RequestBody RegistrationRequest request) {
+        authService.registerUser(request);
+        return ResponseEntity.ok().build();
     }
 }
