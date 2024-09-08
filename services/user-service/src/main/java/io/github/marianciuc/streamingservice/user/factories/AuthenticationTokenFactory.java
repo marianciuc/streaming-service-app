b665919b9cc5b85824b3b136a9fc296db20dc1f5
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:47:52 +0200

    Add AuthenticationTokenFactory interface
    Introduce AuthenticationTokenFactory to centralize token creation. The interface extends Function<Authentication, Token> and will be implemented to generate tokens based on user authentication details.

---
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/factories/AuthenticationTokenFactory.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: AuthenticationTokenFactory.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.user.factories;
+
+import io.github.marianciuc.streamingservice.user.dto.Token;
+import org.springframework.security.core.Authentication;
+
+import java.util.function.Function;
+
+public interface AuthenticationTokenFactory extends Function<Authentication, Token> {
+}
