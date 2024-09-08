39c1a5a15128e30a5e80591543e80767e3925666
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-09 00:02:17 +0200

    Add CORS configuration and register endpoint permission
    Updated SecurityConfig to disable CORS and allow unauthenticated access to the /api/v1/auth/register endpoint. This enhances security by clearly defining permitted requests and simplifying CORS policies.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/config/SecurityConfig.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/config/SecurityConfig.java
@@ -17,10 +17,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.text.ParseException;
 
@@ -34,7 +36,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationConfigurer jwtAuthenticationConfigurer) throws Exception {
         http.apply(jwtAuthenticationConfigurer);
         http
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(config -> config
+                        .requestMatchers("/api/v1/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
