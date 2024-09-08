9def5136b4313864aeda675eabfde3ddfa46319e
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:46:30 +0200

    Refactor SecurityConfig to improve JWT handling
    Replaced existing JWT filter configuration with a more modular and robust approach, incorporating custom token serializers and deserializers. Introduced JwtAuthenticationConfigurer to streamline JWT configuration and added PasswordEncoder bean for password encryption.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/config/SecurityConfig.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/config/SecurityConfig.java
@@ -1,44 +1,74 @@
 package io.github.marianciuc.streamingservice.user.config;
 
-import io.github.marianciuc.jwtsecurity.filters.JsonWebTokenFilter;
-import io.github.marianciuc.streamingservice.user.enums.APIPath;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.crypto.DirectDecrypter;
+import com.nimbusds.jose.crypto.DirectEncrypter;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jose.jwk.OctetSequenceKey;
+import io.github.marianciuc.streamingservice.user.factories.AccessTokenFactory;
+import io.github.marianciuc.streamingservice.user.factories.RefreshTokenFactory;
+import io.github.marianciuc.streamingservice.user.serializers.*;
+import io.github.marianciuc.streamingservice.user.services.impl.TokenAuthenticationUserDetailsService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.text.ParseException;
+
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final AuthenticationProvider authenticationProvider;
-    private final JsonWebTokenFilter jsonWebTokenFilter;
-
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthenticationConfigurer jwtAuthenticationConfigurer) throws Exception {
+        http.apply(jwtAuthenticationConfigurer);
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth ->
-                        auth
-                                .requestMatchers(APIPath.AUTH_V1_CONTROLLER).permitAll()
-                                .anyRequest().authenticated()
+                .authorizeHttpRequests(config -> config
+                        .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-        ;
-
-        http.addFilterBefore(jsonWebTokenFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
+    @Bean
+    public JwtAuthenticationConfigurer jwtAuthenticationConfigurer(
+            @Value("${jwt.access-token-key}") String accessTokenKey,
+            @Value("${jwt.refresh-token-key}") String refreshTokenKey,
+            TokenAuthenticationUserDetailsService tokenAuthenticationUserDetailsService,
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder
+    ) throws ParseException, JOSEException {
 
+        TokenDeserializer accessTokenDeserializer = new AccessJWETokenStringDeserializer(new MACVerifier(OctetSequenceKey.parse(accessTokenKey)));
+        TokenDeserializer refreshTokenDeserializer = new RefreshJWSTokenStringDeserializer(new DirectDecrypter(OctetSequenceKey.parse(refreshTokenKey)));
+        TokenSerializer accessTokenSerializer = new AccessJWSTokenStringSerializer(new MACSigner(OctetSequenceKey.parse(accessTokenKey)));
+        TokenSerializer refreshTokenSerializer = new RefreshJWETokenStringSerializer(new DirectEncrypter(OctetSequenceKey.parse(refreshTokenKey)));
 
+        return new JwtAuthenticationConfigurer()
+                .passwordEncoder(passwordEncoder)
+                .jwtUserDetailsService(tokenAuthenticationUserDetailsService)
+                .credentialsUserDetailsService(userDetailsService)
+                .accessTokenFactory(new AccessTokenFactory())
+                .refreshTokenFactory(new RefreshTokenFactory())
+                .accessTokenStringDeserializer(accessTokenDeserializer)
+                .refreshTokenStringDeserializer(refreshTokenDeserializer)
+                .accessTokenSerializer(accessTokenSerializer)
+                .refreshTokenSerializer(refreshTokenSerializer);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
