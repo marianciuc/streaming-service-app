fe726f7e8d7d4e00037df2a65bd87efa321e55c6
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:47:41 +0200

    Add JsonWebTokenRepository class
    Introduce a new repository class, JsonWebTokenRepository, to the user service module. This class will serve as a placeholder for future implementations related to JSON Web Token (JWT) management in the streaming service application.

---
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/repositories/JsonWebTokenRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: JsonWebTokenRepository.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.user.repositories;
+
+public class JsonWebTokenRepository {
+
+
+
+}
