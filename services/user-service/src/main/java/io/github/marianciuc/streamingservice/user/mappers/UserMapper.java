5aae81091d1230e985ac5663f8faa915721cebc5
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-09 00:02:27 +0200

    Remove unused fromRegistrationRequest method in UserMapper
    Eliminated the fromRegistrationRequest method as it is no longer used and its corresponding import, RegistrationRequest. This change simplifies the UserMapper class by removing redundant and unused code.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/mappers/UserMapper.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/mappers/UserMapper.java
@@ -1,6 +1,5 @@
 package io.github.marianciuc.streamingservice.user.mappers;
 
-import io.github.marianciuc.streamingservice.user.dto.requests.RegistrationRequest;
 import io.github.marianciuc.streamingservice.user.dto.responses.UserResponse;
 import io.github.marianciuc.streamingservice.user.entity.User;
 import io.github.marianciuc.streamingservice.user.enums.Role;
@@ -8,14 +7,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
-
-    public User fromRegistrationRequest(RegistrationRequest request) {
-        return User.builder()
-                .email(request.email())
-                .username(request.username())
-                .build();
-    }
-
     public UserResponse toResponse(User user) {
         return new UserResponse(
                 user.getId(),
