480dad316bf678995099d55f5ceee627158ff721
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-08 19:46:35 +0200

    Add SecurityConfig to content service
    Introduce a new SecurityConfig class to the content service module. This class is annotated with @Configuration and @EnableWebSecurity, setting up the basic security configuration for the content service.

---
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: SecurityConfig.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.content.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
