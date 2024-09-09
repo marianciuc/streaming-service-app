bc96a8dfa7dc0de8fe893846a10795339cc2edd5
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-10 01:52:22 +0200

    Add new tables for video, files, and video uploading statuses
    Introduced new tables: `video`, `files`, and `video_uploading_statuses` to the schema. These tables include necessary fields for handling video metadata, processing states, and uploading statuses.

--- services/media-service/src/main/resources/db/migration/V1__init_database.sql
+++ services/media-service/src/main/resources/db/migration/V1__init_database.sql
@@ -11,5 +11,37 @@ create table if not exists resolutions
 create table if not exists images
 (
     id uuid primary key,
+    content_type varchar(255),
+    content_length bigint,
+    file_name varchar(255),
+    created_at timestamp
+);
 
-)
+create table if not exists video
+(
+    id uuid primary key,
+    content_id uuid,
+    content_type varchar(255),
+    media_type varchar(255),
+    processed_resolutions integer,
+    status varchar(255),
+    master_playlist_path varchar(255)
+);
+
+create table if not exists files
+(
+    id uuid primary key,
+    video_id uuid references video(id),
+    resolution_id uuid references resolutions(id),
+    is_processed boolean,
+    play_list_path varchar(255)
+);
+
+create table if not exists video_uploading_statues
+(
+    id uuid primary key,
+    title varchar(200),
+    message varchar(200),
+    video_id uuid references video(id),
+    type varchar(200)
+);
