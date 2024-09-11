81d5a22cdd48aba4ac215cefa05bb89e3323c226
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:03:14 +0200

    Add KafkaTopics for payment service
    Introduced a new KafkaTopics class to define topic names used in the payment service. This will centralize the topic constants and improve maintainability.

---
+++ services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/kafka/KafkaTopics.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: KafkaTopics.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.payment.kafka;
+
+public class KafkaTopics {
+    public static final String PAYMENT_STATUS_TOPIC = "payment-status-topic";
+    public static final String START_PROCESS_PAYMENT = "start-process-payment-topic";
+}
