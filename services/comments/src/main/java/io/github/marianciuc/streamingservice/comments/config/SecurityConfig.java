0119d638ff6369158322d982cc06a352459b2eea
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:02:09 +0200

    Add SecurityConfig.java to comments service
    Introduced initial SecurityConfig.java file to the comments service. This sets up the basic structure for future security configurations within the comments module. This change does not yet implement any specific security measures.

---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: SecurityConfig.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.config;
+
+public class SecurityConfig {
+}
