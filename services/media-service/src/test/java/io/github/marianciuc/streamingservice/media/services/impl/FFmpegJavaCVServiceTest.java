cb0a238885927e97a15990aaf2fd6dac5f48d5f2
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:02:51 +0200

    Update test to handle video compression and storage
    Refactored `FFmpegJavaCVServiceTest` to replace mock multipart file handling with `VideoStorageService`. Added new dependencies and revised the test method to verify video compression and upload functionality.

--- services/media-service/src/test/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegJavaCVServiceTest.java
+++ services/media-service/src/test/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegJavaCVServiceTest.java
@@ -8,22 +8,22 @@
 
 package io.github.marianciuc.streamingservice.media.services.impl;
 
-import io.github.marianciuc.streamingservice.media.entity.Resolution;
+import io.github.marianciuc.streamingservice.media.dto.ResolutionDto;
+import io.github.marianciuc.streamingservice.media.services.VideoStorageService;
+import io.minio.errors.MinioException;
 import lombok.extern.slf4j.Slf4j;
-import org.bytedeco.javacv.FrameGrabber;
-import org.bytedeco.javacv.FrameRecorder;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -31,41 +31,24 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 @Slf4j
 public class FFmpegJavaCVServiceTest {
+
     @Mock
-    private MultipartFile multipartFileMock;
+    private VideoStorageService videoStorageService;
 
     @InjectMocks
     private FFmpegJavaCVService ffmpegJavaCVService;
 
     private File testInputFile;
 
-
-    @BeforeEach
-    public void setUp() throws IOException {
-        testInputFile = new File("src/test/resources/test_video.mp4");
-        log.info("{}", testInputFile.exists());
-        log.info("{}", testInputFile.getAbsolutePath());
-        log.info("{}", testInputFile.length());
-
-        FileInputStream fileInputStream = new FileInputStream(testInputFile);
-
-        when(multipartFileMock.getOriginalFilename()).thenReturn(testInputFile.getName());
-        when(multipartFileMock.getSize()).thenReturn(testInputFile.length());
-        when(multipartFileMock.getInputStream()).thenReturn(fileInputStream);
-        when(multipartFileMock.getContentType()).thenReturn("video/mp4");
-        when(multipartFileMock.isEmpty()).thenReturn(false);
-    }
-
-
     @Test
-    public void testCompressVideoWithSuccess() {
-        Resolution resolution = Resolution.builder()
-                .width(640)
-                .height(480)
-                .bitrate(400_000)
-                .build();
-
-//        byte[] compressedVideo = ffmpegJavaCVService.compressVideo(multipartFileMock, resolution);
-//        assertNotNull(compressedVideo, "The compressed video should not be null.");
+    void compressVideoAndUploadToStorage() throws IOException, MinioException, NoSuchAlgorithmException, InvalidKeyException {
+        ResolutionDto resolutionDto = new ResolutionDto(UUID.randomUUID(), "1920", "1080", 720, 1280, 3000);
+        UUID videoId = UUID.randomUUID();
+        File tempFile = Files.createTempFile("input", ".mp4").toFile();
+        when(videoStorageService.assembleVideo(videoId)).thenReturn(Files.newInputStream(Path.of(tempFile.getAbsolutePath())));
+        String result = ffmpegJavaCVService.compressVideoAndUploadToStorage(resolutionDto, videoId);
+        assertNotNull(result);
+        verify(videoStorageService, atLeastOnce()).uploadFile(anyString(), any(), anyLong(), anyString());
+        tempFile.delete();
     }
 }
