8b538ed84ee9e7a4db26ea4b250d05d074db9ddc
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:02:45 +0200

    Create CustomerClient interface
    Added a new interface CustomerClient to the comments module. This interface will be used to define methods for interactions with the customer service.

---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/clients/CustomerClient.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: CustomerClient.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.clients;
+
+public interface CustomerClient {
+}
