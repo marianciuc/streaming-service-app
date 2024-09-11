b5210e2afb6b535f065b0055e89699135076ae95
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-11 23:30:48 +0200

    Refactor Refund entity to use JPA annotations
    Replaced MongoDB-specific annotations with JPA annotations in the Refund entity. Added fields for currency, status, and a relation to Transaction.

--- services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/entity/Refund.java
+++ services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/entity/Refund.java
@@ -1,25 +1,47 @@
 package io.github.marianciuc.streamingservice.payment.entity;
 
-import jakarta.persistence.Id;
+import io.github.marianciuc.streamingservice.payment.enums.Currency;
+import io.github.marianciuc.streamingservice.payment.enums.RefundStatus;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Document
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@Entity
+@Table(name = "refunds")
 public class Refund {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private Long amountInCents;
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "currency")
+    private Currency currency;
+
+    @Column(name = "stripe_refund_id")
     private String stripeRefundId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
     private RefundStatus status;
+
+    @Column(name = "created_at")
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "transaction_id", referencedColumnName = "id")
+    private Transaction transaction;
 }
