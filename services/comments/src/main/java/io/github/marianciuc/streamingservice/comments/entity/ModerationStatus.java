f150979baf20c44adf177db713198c2b4032b5bb
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:03:21 +0200

    Create ModerationStatus enum
    Added a new enum, ModerationStatus, under the comments entity package. This enum is currently empty and will be used to define various moderation statuses for comments in future updates.

---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/entity/ModerationStatus.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: ModerationStatus.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.entity;
+
+public enum ModerationStatus {
+}
