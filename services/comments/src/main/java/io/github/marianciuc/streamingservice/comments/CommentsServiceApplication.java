f47fdbfcb2cd49adf01fa41a4ce2d4a7edc450dd
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:01:14 +0200

    Add CommentsServiceApplication with basic structure
    Created the main application class for the comments service using Spring Boot. Also included a basic test to ensure the application context loads correctly.

---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/CommentsServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.marianciuc.streamingservice.comments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentsServiceApplication.class, args);
+	}
+
+}
---
+++ services/comments/src/test/java/io/github/marianciuc/streamingservice/comments/CommentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marianciuc.streamingservice.comments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
