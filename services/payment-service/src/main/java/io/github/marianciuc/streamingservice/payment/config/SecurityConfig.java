897336b64c30c416ff025ea6a63cffc40540a320
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:02:04 +0200

    Refactor SecurityConfig to support Stripe integration
    Added Stripe configuration with API key and removed JSON Web Token filter. Adjusted HTTP security settings for cors and authorization.

--- services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/config/SecurityConfig.java
+++ services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/config/SecurityConfig.java
@@ -1,28 +1,33 @@
 package io.github.marianciuc.streamingservice.payment.config;
 
 
-import io.github.marianciuc.jwtsecurity.service.JsonWebTokenFilter;
+import com.stripe.Stripe;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Configuration
 public class SecurityConfig {
-    private final JsonWebTokenFilter jsonWebTokenService;
+
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   @Value("${stripe.api.key}") String apiKey) throws Exception {
+        Stripe.apiKey = apiKey;
         http
+                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jsonWebTokenService, UsernamePasswordAuthenticationFilter.class);
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 }
