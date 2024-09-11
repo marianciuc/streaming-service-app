ac87f7635c6624b7bd09d69500523386da43132c
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:01:23 +0200

    Create ContentClient interface
    This commit introduces the ContentClient interface for the comments service. This new interface will serve as a contract for communication between the comments service and the content-related aspects of the application.

---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/clients/ContentClient.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: ContentClient.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.clients;
+
+public interface ContentClient {
+}
