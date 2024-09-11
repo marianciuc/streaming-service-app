a7daacaefe7ec034465c1cbf69851e4293751d55
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:01:44 +0200

    Add Review functionality with controller, entity, and DTO
    This commit introduces the initial structure for a Review feature. It includes a `ReviewController` for handling API requests, a `ReviewRate` entity for database representation, and a `ReviewDto` for data transfer. The new files set the foundation for handling reviews within the streaming service application.

---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/controllers/ReviewController.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: ReviewController.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/reviews")
+public class ReviewController {
+}
---
+++ services/comments/src/main/java/io/github/marianciuc/streamingservice/comments/dto/ReviewDto.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: ReviewDto.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.dto;
+
+public record ReviewDto() {
+}
