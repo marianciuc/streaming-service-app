a4d698d90a802d2a8d3e740b0cbc03c31daf6db1
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-12 00:03:08 +0200

    Add KafkaMessageModerationConsumer component
    Introduce a new KafkaMessageModerationConsumer class in the moderation service. This sets up the infrastructure for handling Kafka messages within the moderation context.

---
+++ services/moderation-service/src/main/java/io/github/marianciuc/streamingservice/moderation/kafka/KafkaMessageModerationConsumer.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: KafkaMessageModerationConsumer.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.moderation.kafka;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaMessageModerationConsumer {
+
+}
