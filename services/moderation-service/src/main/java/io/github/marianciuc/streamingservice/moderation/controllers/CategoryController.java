d709f522fd76e0305eaa08a42f902d8933e092ad
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:18:25 +0200

    Add authorization to addCategory endpoint
    Implemented `@PreAuthorize` annotation on `addCategory` method to restrict access to users with 'ROLE_MODERATOR' or 'ROLE_ADMIN'. This enhancement ensures that only authorized roles can add new categories.

--- services/moderation-service/src/main/java/io/github/marianciuc/streamingservice/moderation/controllers/CategoryController.java
+++ services/moderation-service/src/main/java/io/github/marianciuc/streamingservice/moderation/controllers/CategoryController.java
@@ -12,6 +12,7 @@ import io.github.marianciuc.streamingservice.moderation.dto.CategoryDto;
 import io.github.marianciuc.streamingservice.moderation.services.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,7 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('ROLE_MODERATOR', 'ROLE_ADMIN')")
     public ResponseEntity<CategoryDto> addCategory(@RequestBody CategoryDto categoryDto) {
         return ResponseEntity.ok(categoryService.add(categoryDto));
     }
