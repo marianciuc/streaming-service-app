41a3fdb457ebf572f8b374b05363a2b77ca544e2
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:18:31 +0200

    Enhance password validation in ChangePasswordRequest
    Add @NotBlank constraints to ensure neither old nor new passwords are blank. Introduce @ValidPassword annotation for new password to enforce stronger validation rules.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/dto/requests/ChangePasswordRequest.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/dto/requests/ChangePasswordRequest.java
@@ -8,8 +8,18 @@
 
 package io.github.marianciuc.streamingservice.user.dto.requests;
 
+import io.github.marianciuc.streamingservice.user.validation.ValidPassword;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
 public record ChangePasswordRequest(
+
+        @NotBlank(message = "Old password must not be blank")
         String oldPassword,
+
+        @NotBlank(message = "New password must not be blank")
+        @ValidPassword
         String newPassword
 ) {
 }
