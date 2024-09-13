4575e2efe3f68055ea3e46b75d72247a0df01025
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:20:01 +0200

    Refactor UserResponse and add static toResponse method
    Renamed 'uuid' to 'id' for better clarity and removed 'passwordHash' from UserResponse. Added a static method 'toResponse' to convert User entity to UserResponse, improving readability and usage in the codebase.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/dto/responses/UserResponse.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/dto/responses/UserResponse.java
@@ -9,17 +9,26 @@
 package io.github.marianciuc.streamingservice.user.dto.responses;
 
 
+import io.github.marianciuc.streamingservice.user.entity.User;
 import io.github.marianciuc.streamingservice.user.enums.RecordStatus;
 import io.github.marianciuc.streamingservice.user.enums.Role;
 
 import java.util.UUID;
 
 public record UserResponse(
-        UUID uuid,
+        UUID id,
         String email,
-        String passwordHash,
         RecordStatus recordStatus,
         Role role,
         Boolean isBanned
 ) {
+    public static UserResponse toResponse(User user) {
+        return new UserResponse(
+                user.getId(),
+                user.getEmail(),
+                user.getRecordStatus(),
+                Role.valueOf(user.getRole()),
+                user.getIsBanned()
+        );
+    }
 }
