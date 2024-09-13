1733b2846ece8541157518bfb65496d28b3acd7a
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-13 02:11:22 +0200

    Add RefundController to manage refund-related endpoints
    Introduced a new RefundController class annotated with @RestController. This sets up the base for handling refund operations under the /api/v1/refunds endpoint.

---
+++ services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/controller/RefundController.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: RefundController.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.payment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/refunds")
+public class RefundController {
+
+}
