16c497b22161650020ea2c946ab3d35e6f5035b7
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:19:55 +0200

    Add UserRateMessage record to Kafka messages package
    Create a new `UserRateMessage` record in the `kafka.messages` package of the content service. This setup will allow handling user rating messages within the streaming service application.

---
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/kafka/messages/UserRateMessage.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: UserRateMessage.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.content.kafka.messages;
+
+public record UserRateMessage() {
+}
