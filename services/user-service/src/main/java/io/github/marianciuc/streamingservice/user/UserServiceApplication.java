9c91a4e7120a982b85a44a6e689f4ac8891ba952
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-13 02:11:38 +0200

    Fix whitespace in main method signature
    Corrected an extra unnecessary space in the `main` method signature. This change improves code formatting and maintains coding standards.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/UserServiceApplication.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/UserServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class UserServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
