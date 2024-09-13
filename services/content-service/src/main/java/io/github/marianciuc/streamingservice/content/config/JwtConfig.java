e2b6f68003a5b17ef485bf66f89b56b9d48213ba
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 01:00:25 +0200

    Add JwtConfig class for JWT verification
    Introduced JwtConfig configuration class to handle the JWT verifier bean creation. It fetches the RSA public key from the user-service to set up the RSASSAVerifier.

---
+++ services/content-service/src/main/java/io/github/marianciuc/streamingservice/content/config/JwtConfig.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: JwtConfig.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.content.config;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSVerifier;
+import com.nimbusds.jose.crypto.RSASSAVerifier;
+import com.nimbusds.jose.jwk.RSAKey;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.text.ParseException;
+
+@Slf4j
+@Configuration
+public class JwtConfig {
+
+    @Bean
+    public JWSVerifier jwsVerifier(WebClient webClient) throws ParseException, JOSEException {
+
+        String publicKey = webClient.get()
+                .uri("http://user-service/jwt/public")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        assert publicKey != null;
+        RSAKey publicJWK = RSAKey.parse(publicKey);
+
+        return new RSASSAVerifier(publicJWK);
+    }
+}
