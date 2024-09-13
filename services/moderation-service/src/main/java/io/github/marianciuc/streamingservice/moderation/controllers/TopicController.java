bb6aa967f40465f3a6ea7d8f09cc4cea06bfe3e0
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:23:46 +0200

    Add createTopic endpoint to TopicController
    Introduced a new POST endpoint to handle topic creation requests. The new endpoint accepts a CreateTopicRequest payload, validates it, and responds with the generated UUID of the new topic.

--- services/moderation-service/src/main/java/io/github/marianciuc/streamingservice/moderation/controllers/TopicController.java
+++ services/moderation-service/src/main/java/io/github/marianciuc/streamingservice/moderation/controllers/TopicController.java
@@ -8,10 +8,17 @@
 
 package io.github.marianciuc.streamingservice.moderation.controllers;
 
+import io.github.marianciuc.streamingservice.moderation.dto.CreateTopicRequest;
 import io.github.marianciuc.streamingservice.moderation.services.TopicMessagesService;
 import io.github.marianciuc.streamingservice.moderation.services.TopicService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController("/api/v1/moderation/topics")
 @RequiredArgsConstructor
@@ -19,5 +26,12 @@ public class TopicController {
 
     private final TopicService topicService;
     private final TopicMessagesService topicMessagesService;
+
+    @PostMapping
+    public ResponseEntity<UUID> createTopic(@Valid @RequestBody CreateTopicRequest createTopicRequest) {
+        return ResponseEntity.ok(topicService.createTopic(createTopicRequest));
+    }
+
+
 
 }
