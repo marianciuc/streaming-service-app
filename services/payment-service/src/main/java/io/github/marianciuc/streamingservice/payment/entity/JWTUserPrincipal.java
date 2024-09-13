bb0fb352207e1572943c1faa37b6c7ae62213da1
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-13 02:08:21 +0200

    Add JWT security features to payment service
    Implemented JWT authentication via a new JWTFilter and configuration class JwtConfig. Added entity JWTUserPrincipal for handling authenticated user details.

---
+++ services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/entity/JWTUserPrincipal.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: JWTUserPrincipal.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.payment.entity;
+
+import io.github.marianciuc.streamingservice.payment.dto.common.Token;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.UUID;
+
+@Getter
+public class JWTUserPrincipal extends User {
+
+    private final Token token;
+    private final UUID userId;
+
+    public JWTUserPrincipal(Token jwtToken) {
+        super(jwtToken.subject(), "", true, true, true, true,
+                jwtToken.roles().stream().map(SimpleGrantedAuthority::new).toList());
+        this.token = jwtToken;
+        this.userId = jwtToken.userId();
+    }
+}
