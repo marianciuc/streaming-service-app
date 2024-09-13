8fb0e877b1336d0f928d602d60a167556dd846d1
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:20:15 +0200

    Add TokenDeserializer interfaces to customer and comments services
    Introduced TokenDeserializer interfaces in both customer and comments services for standardized token deserialization. This will facilitate consistent token handling across different modules. The interfaces extend the Function interface, converting a String to a Token object.

---
+++ services/customer-service/src/main/java/io/github/marianciuc/streamingservice/customer/security/serialization/TokenDeserializer.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: TokenDeserializer.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.customer.security.serialization;
+
+
+import io.github.marianciuc.streamingservice.customer.security.dto.Token;
+
+import java.util.function.Function;
+
+public interface TokenDeserializer extends Function<String, Token> {
+}
