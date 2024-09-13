c97c81f4d1e02f9b32fc18204c652b2a24d210b4
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-13 02:11:11 +0200

    Update address logic with authentication check
    This commit enhances the `updateAddress` method by adding a check for an authenticated `JWTUserPrincipal`. It only updates the address fields if provided in the request and ensures that the user is authenticated before proceeding. If the user is not authenticated, it throws a runtime exception.

--- services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/service/impl/AddressServiceImpl.java
+++ services/payment-service/src/main/java/io/github/marianciuc/streamingservice/payment/service/impl/AddressServiceImpl.java
@@ -10,9 +10,11 @@ package io.github.marianciuc.streamingservice.payment.service.impl;
 
 import io.github.marianciuc.streamingservice.payment.dto.common.AddressDto;
 import io.github.marianciuc.streamingservice.payment.entity.Address;
+import io.github.marianciuc.streamingservice.payment.entity.JWTUserPrincipal;
 import io.github.marianciuc.streamingservice.payment.repository.AddressRepository;
 import io.github.marianciuc.streamingservice.payment.service.AddressService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -38,8 +40,19 @@ public class AddressServiceImpl implements AddressService {
     }
 
     @Override
-    public void updateAddress(AddressDto address) {
-
+    public void updateAddress(AddressDto req) {
+        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof JWTUserPrincipal jwtUserPrincipal) {
+            Address address = repository.findById(req.id()).orElseThrow(() -> new RuntimeException("Address not found"));
+            if (!req.city().isEmpty()) address.setCity(req.city());
+            if (!req.state().isEmpty()) address.setState(req.state());
+            if (!req.line1().isEmpty()) address.setLine1(req.line1());
+            if (!req.line2().isEmpty()) address.setLine2(req.line2());
+            if (!req.country().isEmpty()) address.setCountry(req.country());
+            if (!req.postalCode().isEmpty()) address.setPostalCode(req.postalCode());
+            repository.save(address);
+        } else {
+            throw new RuntimeException("User not authenticated");
+        }
     }
 
     @Override
