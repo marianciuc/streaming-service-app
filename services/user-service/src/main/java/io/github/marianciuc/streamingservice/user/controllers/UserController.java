534d30b27deba47e75b066d1a10a962206557992
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 00:21:22 +0200

    Update UserController with simplified endpoints
    Replaced APIPath references with direct URL paths for clarity and consistency. Updated endpoint mappings for change password, ban user, and change role operations for better readability and maintainability.

--- services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/UserController.java
+++ services/user-service/src/main/java/io/github/marianciuc/streamingservice/user/controllers/UserController.java
@@ -1,13 +1,11 @@
 package io.github.marianciuc.streamingservice.user.controllers;
 
 import io.github.marianciuc.streamingservice.user.dto.requests.ChangePasswordRequest;
-import io.github.marianciuc.streamingservice.user.enums.APIPath;
 import io.github.marianciuc.streamingservice.user.enums.Role;
 import io.github.marianciuc.streamingservice.user.services.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,7 +18,7 @@ import java.util.UUID;
  * It defines various methods for changing passwords, creating employees, banning users, and changing user roles.
  */
 @RestController
-@RequestMapping(APIPath.USER_V1_CONTROLLER)
+@RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -32,7 +30,7 @@ public class UserController {
      * @param request the ChangePasswordRequest object containing the old and new passwords
      * @return a ResponseEntity with a status of 200 OK if the password change was successful, or the appropriate error response if it failed
      */
-    @PutMapping(APIPath.CHANGE_PASSWORD)
+    @PutMapping("/change-password")
     public ResponseEntity<Void> changePassword(@RequestBody @Validated ChangePasswordRequest request){
         userService.changePassword(request);
         return ResponseEntity.ok().build();
@@ -45,8 +43,8 @@ public class UserController {
      * @return the unique identifier of the saved user after banning
      */
     @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    @PutMapping(APIPath.BAN_USER)
-    public ResponseEntity<Void> banUser(@RequestParam("user-id") UUID userId) {
+    @PutMapping("/ban/{user-id}")
+    public ResponseEntity<Void> banUser(@PathVariable("user-id") UUID userId) {
         userService.banUser(userId);
         return ResponseEntity.ok().build();
     }
@@ -58,8 +56,8 @@ public class UserController {
      * @param userId the unique identifier of the user
      * @return a ResponseEntity with a status of 200 OK if the role change was successful, or an error response if it failed
      */
-    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_SERVICE')")
-    @PutMapping(APIPath.CHANGE_ROLE)
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    @PutMapping("/role")
     public ResponseEntity<Void> changeRole(
             @RequestParam("role") Role role,
             @RequestParam("user-id") UUID userId
