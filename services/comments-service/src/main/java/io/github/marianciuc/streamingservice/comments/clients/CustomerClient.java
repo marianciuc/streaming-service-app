c74ab3206738f91f724ec1f228fa93c253c77c82
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 02:03:11 +0200

    Add Feign client in CustomerClient for customer-service
    Introduced a Feign client interface for integration with the customer-service. This includes a method to retrieve customer details by ID, leveraging Spring Cloud OpenFeign and component scanning.

--- services/comments-service/src/main/java/io/github/marianciuc/streamingservice/comments/clients/CustomerClient.java
+++ services/comments-service/src/main/java/io/github/marianciuc/streamingservice/comments/clients/CustomerClient.java
@@ -8,5 +8,18 @@
 
 package io.github.marianciuc.streamingservice.comments.clients;
 
+import io.github.marianciuc.streamingservice.comments.dto.CustomerDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@FeignClient(name = "customer-service", url = "${application.config.customer-url}")
+@Component
 public interface CustomerClient {
+    @GetMapping("/{customer-id}")
+    Optional<CustomerDto> getCustomerById(@PathVariable("customer-id") UUID customerId);
 }
