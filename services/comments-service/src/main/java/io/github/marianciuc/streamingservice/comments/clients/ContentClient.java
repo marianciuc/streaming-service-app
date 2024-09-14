404c8562866676482da8485ef2a9551d7a922692
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 02:03:15 +0200

    Add Feign client for Content service
    Introduced a Feign client in `ContentClient` to interact with the Content service. This includes a method `existsById` for checking the existence of content by its ID. Additionally, necessary imports were added to facilitate the Feign client functionality.

--- services/comments-service/src/main/java/io/github/marianciuc/streamingservice/comments/clients/ContentClient.java
+++ services/comments-service/src/main/java/io/github/marianciuc/streamingservice/comments/clients/ContentClient.java
@@ -8,5 +8,18 @@
 
 package io.github.marianciuc.streamingservice.comments.clients;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@FeignClient(name = "content-service", url = "${application.config.content-url}")
+@Component
 public interface ContentClient {
+
+    @GetMapping("/{id}")
+    Boolean existsById(@PathVariable UUID id);
 }
