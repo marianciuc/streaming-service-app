cbb1aaedc2d4c4371050c9c2920adf168fbe1e84
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-14 02:02:44 +0200

    Add KafkaMessageModerationProducer class
    Introduce a new KafkaMessageModerationProducer class for handling message production to Kafka in the comments service. This class is annotated with @Component to be managed by Spring's dependency injection framework.

---
+++ services/comments-service/src/main/java/io/github/marianciuc/streamingservice/comments/kafka/KafkaMessageModerationProducer.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024  Vladimir Marianciuc. All Rights Reserved.
+ *
+ * Project: STREAMING SERVICE APP
+ * File: KafkaMessageModerationProducer.java
+ *
+ */
+
+package io.github.marianciuc.streamingservice.comments.kafka;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaMessageModerationProducer {
+}
