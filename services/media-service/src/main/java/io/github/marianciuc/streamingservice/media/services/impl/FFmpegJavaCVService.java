bb59df25c2fe74a7344d84eef8fe8fdaf918cccb
Vladimir Marianciuc <boosth14@gmail.com>
2024-09-20 22:53:01 +0200

    Refactor FFmpegJavaCVService to modularize playlist handling
    Separated playlist management into a dedicated PlaylistService. Simplified chunk upload logic by cleaning up redundant code and improving method signatures. Enhanced error handling and code readability for video compression and upload processes.

--- services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegJavaCVService.java
+++ services/media-service/src/main/java/io/github/marianciuc/streamingservice/media/services/impl/FFmpegJavaCVService.java
@@ -9,9 +9,10 @@ package io.github.marianciuc.streamingservice.media.services.impl;
 
 import io.github.marianciuc.streamingservice.media.dto.ResolutionDto;
 import io.github.marianciuc.streamingservice.media.exceptions.CompressingException;
+import io.github.marianciuc.streamingservice.media.exceptions.VideoStorageUploadException;
+import io.github.marianciuc.streamingservice.media.services.PlaylistService;
 import io.github.marianciuc.streamingservice.media.services.VideoCompressingService;
 import io.github.marianciuc.streamingservice.media.services.VideoStorageService;
-import io.minio.errors.MinioException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bytedeco.ffmpeg.global.avcodec;
@@ -25,11 +26,7 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
-
-import static io.github.marianciuc.streamingservice.media.services.VideoStorageService.*;
 
 @Service
 @Slf4j
@@ -46,20 +43,18 @@ public class FFmpegJavaCVService implements VideoCompressingService {
     private static final String OUTPUT_FORMAT = "hls";
     private static final int CHUNK_DURATION_IN_SECONDS = 10;
     private static final String HLS_SEGMENT_FILENAME = "segment%d.ts";
-
     private static final String CONTENT_TYPE_VIDEO_MP2T = "video/mp2t";
-    private static final String CONTENT_TYPE_APPLICATION_X_MPEGURL = "application/x-mpegURL";
-    private static final String PATH_PLAYLIST_TEMPLATE = CONTENT_PATH + "hls/%s/index.m3u8";
 
     private final VideoStorageService videoStorageService;
+    private final PlaylistService playlistService;
 
     @Override
     public String compressVideoAndUploadToStorage(ResolutionDto resolution, UUID id) throws CompressingException {
         File inputFile = null;
 
         try {
-            inputFile = createTempFile(PREFIX_INPUT_FILE, "input-temp-file");
-            Files.copy(videoStorageService.assembleVideo(id), Path.of(inputFile.getAbsolutePath()),
+            inputFile = createTempFile();
+            Files.copy(videoStorageService.assembleVideoTemporaryVideoFile(id), Path.of(inputFile.getAbsolutePath()),
                     StandardCopyOption.REPLACE_EXISTING);
             return executeCompression(inputFile.getAbsolutePath(), resolution, id);
         } catch (IOException e) {
@@ -70,9 +65,6 @@ public class FFmpegJavaCVService implements VideoCompressingService {
     }
 
     private String executeCompression(String inputFilePath, ResolutionDto resolution, UUID id) {
-        log.info("Starting video compression...");
-        log.info("Input file: {}", inputFilePath);
-
         try (FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFilePath);
              ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
 
@@ -82,30 +74,28 @@ public class FFmpegJavaCVService implements VideoCompressingService {
 
             Frame frame;
             FFmpegFrameRecorder recorder = null;
-            int chunkCount = 0;
-            StringBuilder playlistContent = new StringBuilder("#EXTM3U\n");
+            StringBuilder resolutionPlaylist = playlistService.generateResolutionPlaylist();
+            int chunkCounter = 0;
 
             while ((frame = grabber.grab()) != null) {
                 if (recorder == null || recorder.getFrameNumber() >= totalFramesPerChunk) {
                     if (recorder != null) {
                         recorder.stop();
-                        uploadChunkAndResetStream(outputStream, resolution, id, playlistContent, chunkCount);
-                        chunkCount++;
+                        uploadChunkAndResetStream(outputStream, resolution, id, resolutionPlaylist, chunkCounter);
+                        chunkCounter++;
                     }
-
                     recorder = initializeRecorder(outputStream, resolution, frameRate, grabber);
                 }
-
                 recorder.record(frame);
             }
 
             if (recorder != null) {
                 recorder.stop();
-                uploadChunkAndResetStream(outputStream, resolution, id, playlistContent, chunkCount);
+                uploadChunkAndResetStream(outputStream, resolution, id, resolutionPlaylist, chunkCounter);
             }
 
             grabber.stop();
-            return savePlaylist(id, playlistContent.toString(), resolution.height());
+            return playlistService.buildResolutionPlaylist(id, resolutionPlaylist, resolution);
         } catch (Exception e) {
             log.error(FRAME_GRABBING_ERROR_MSG, e);
             throw new CompressingException(FRAME_GRABBING_ERROR_MSG, e);
@@ -134,31 +124,15 @@ public class FFmpegJavaCVService implements VideoCompressingService {
         recorder.setOption("hls_segment_filename", HLS_SEGMENT_FILENAME);
     }
 
-    private void uploadChunkAndResetStream(ByteArrayOutputStream outputStream, ResolutionDto resolution, UUID fileId, StringBuilder playlistContent, int chunkCount) throws IOException, MinioException, NoSuchAlgorithmException, InvalidKeyException {
-        String objectName = String.format(PATH_CHUNK_TEMPLATE, resolution.height(), fileId, chunkCount);
-
-        try (InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray())) {
-            videoStorageService.uploadFile(objectName, inputStream, outputStream.size(), CONTENT_TYPE_VIDEO_MP2T);
-
-            playlistContent.append("#EXTINF:").append(CHUNK_DURATION_IN_SECONDS).append(",\n")
-                    .append(objectName)
-                    .append("\n");
-        }
+    private void uploadChunkAndResetStream(ByteArrayOutputStream outputStream, ResolutionDto resolution, UUID fileId,
+                                           StringBuilder playlist, int chunkNumber) throws VideoStorageUploadException {
+        String objectName = videoStorageService.uploadVideoSegment(outputStream, fileId, resolution, chunkNumber, CONTENT_TYPE_VIDEO_MP2T);
+        playlistService.appendResolutionPlaylist(objectName, playlist, CHUNK_DURATION_IN_SECONDS);
         outputStream.reset();
     }
 
-    private String savePlaylist(UUID id, String playlistContent, int resolutionHeight) throws MinioException, IOException, NoSuchAlgorithmException, InvalidKeyException {
-        String objectName = String.format(PATH_PLAYLIST_TEMPLATE, id + "_" + resolutionHeight);
-
-        try (InputStream playlistStream = new ByteArrayInputStream(playlistContent.getBytes())) {
-            videoStorageService.uploadFile(objectName, playlistStream, playlistContent.length(), CONTENT_TYPE_APPLICATION_X_MPEGURL);
-        }
-
-        return objectName;
-    }
-
-    private File createTempFile(String prefix, String fileName) throws IOException {
-        return File.createTempFile(prefix + fileName, TEMP_FILE_SUFFIX).toPath().toFile();
+    private File createTempFile() throws IOException {
+        return File.createTempFile(FFmpegJavaCVService.PREFIX_INPUT_FILE + "input-temp-file", TEMP_FILE_SUFFIX).toPath().toFile();
     }
 
     private void deleteFile(File file) {
